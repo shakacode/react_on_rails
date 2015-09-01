c39206eca3ac5b63c2c6ab6fadf1be9af399d38b
Justin Gordon <justin.gordon@gmail.com>
2015-09-01 01:17:22 -0700

    Updated version to 0.1.1
    * Added *.gem to gitignore
    * Remoed unnecessary spec example

--- spec/react_on_rails_spec.rb
+++ spec/react_on_rails_spec.rb
@@ -4,8 +4,4 @@ describe ReactOnRails do
   it 'has a version number' do
     expect(ReactOnRails::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
