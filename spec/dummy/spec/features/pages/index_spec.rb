d52f668d30bd2a54c6ad9dcbef1fa554b712724d
Mario Perez <mapreal19@gmail.com>
2015-10-05 10:10:14 +0200

    Added integration tests for the index page

---
+++ spec/dummy/spec/features/pages/index_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+shared_examples "React Component" do |dom_selector|
+  scenario { is_expected.to have_css dom_selector }
+
+  scenario "changes text" do
+    new_text = "Hey there!"
+
+    within(dom_selector) do
+      find("input").set new_text
+      within("h3") do
+        is_expected.to have_content new_text
+      end
+    end
+  end
+end
+
+feature "Pages/Index", js: true do
+  subject { page }
+
+  background do
+    visit root_path
+  end
+
+  context "Server Rendered/Cached React/Redux Component" do
+    include_examples "React Component", "div#ReduxApp-react-component-0"
+  end
+
+  context "Server Rendered/Cached React Component Without Redux" do
+    include_examples "React Component", "div#HelloWorld-react-component-1"
+  end
+
+  context "Simple Client Rendered Component" do
+    include_examples "React Component", "div#HelloWorldApp-react-component-2"
+
+    context "same component with different props" do
+      include_examples "React Component", "div#HelloWorldApp-react-component-3"
+    end
+  end
+
+  context "Simple Component Without Redux" do
+    include_examples "React Component", "div#HelloWorld-react-component-4"
+    include_examples "React Component", "div#HelloWorldES5-react-component-5"
+  end
+
+  context "Non-React Component" do
+    scenario { is_expected.to have_content "Time to visit Maui" }
+  end
+end
