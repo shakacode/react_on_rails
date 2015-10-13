e02477279d34e06abea1b075356c0ab21c54db92
Justin Gordon <justin.gordon@gmail.com>
2015-10-12 20:27:10 -0700

    Added tests for server rendering and console log
    * Using request specs and Nokogiri to check for expected results.

---
+++ spec/dummy/spec/requests/console_logging_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe "Server Error Logging" do
+  it "has server log messages in the script generated" do
+    get server_side_log_throw_path
+    html_nodes = Nokogiri::HTML(response.body)
+
+    expected = <<-JS
+console.log.apply(console, ["[SERVER] RENDERED HelloWorldWithLogAndThrow with data_variable __helloWorldWithLogAndThrowData0__ to dom node with id: HelloWorldWithLogAndThrow-react-component-0"]);
+console.log.apply(console, ["[SERVER] console.log in HelloWorld"]);
+console.warn.apply(console, ["[SERVER] console.warn in HelloWorld"]);
+console.error.apply(console, ["[SERVER] console.error in HelloWorld"]);
+console.error.apply(console, ["[SERVER] Exception in rendering!"]);
+console.error.apply(console, ["[SERVER] message: throw in HelloWorldContainer"]);
+console.error.apply(console, ["[SERVER] stack: Error: throw in HelloWorldContainer\n    at HelloWorldWithLogAndThrow
+    JS
+
+    expected_lines = expected.split("\n")
+
+    script_node = html_nodes.css("script")[2]
+    expected_lines.each do |line|
+      expect(script_node.inner_text).to include(line)
+    end
+  end
+end
