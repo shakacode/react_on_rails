52306dd6e0248d2a74f46e125c0a3cd654db23ef
Rob Wise <robert.wise@outlook.com>
2015-10-19 02:27:06 -0400

    Tune up Coveralls and SimpleCov

--- spec/dummy/spec/rails_helper.rb
+++ spec/dummy/spec/rails_helper.rb
@@ -1,12 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= "test"
-require "simplecov"
-# Using a command name along with the test_env_number prevents results from
-# getting clobbered by other test suites
-SimpleCov.command_name "dummy-specs" + (ENV["TEST_ENV_NUMBER"] || "")
-SimpleCov.start do
-  coverage_dir("../../coverage")
-end
+
+require "simplecov_helper"
 require File.expand_path("../../config/environment", __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
--- spec/react_on_rails_spec.rb
+++ spec/react_on_rails_spec.rb
@@ -1,9 +1,4 @@
-require "simplecov"
-# Not necessary to explicitly start SimpleCov here because of presence of
-# the .simplecov file
-# Using a command name prevents results from getting clobbered by other test
-# suites
-SimpleCov.command_name "gem-specs"
+require "simplecov_helper"
 require "spec_helper"
 
 describe ReactOnRails do
