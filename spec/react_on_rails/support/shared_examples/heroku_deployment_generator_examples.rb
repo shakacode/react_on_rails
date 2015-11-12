35fa4bc8fab18ca8e8ef108b1221f11024c34e47
Rob Wise <robert.wise@outlook.com>
2015-11-12 02:08:01 -0500

    Add unicorn

--- spec/react_on_rails/support/shared_examples/heroku_deployment_generator_examples.rb
+++ spec/react_on_rails/support/shared_examples/heroku_deployment_generator_examples.rb
@@ -2,11 +2,13 @@ shared_examples "heroku_deployment" do
   it "should add heroku deployment files" do
     assert_file("Procfile")
     assert_file(".buildpacks")
+    assert_file("config/unicorn.rb")
   end
 
   it "should add heroku production gems" do
     assert_file("Gemfile") do |contents|
       assert_match("gem 'rails_12factor', group: :production", contents)
+      assert_match("gem 'unicorn'", contents)
     end
   end
 end
