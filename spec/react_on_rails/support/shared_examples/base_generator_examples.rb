220254266fab06832e64e8b2fab30358c069a375
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2015-11-26 17:33:08 +1000

    fix linting rerror

--- spec/react_on_rails/support/shared_examples/base_generator_examples.rb
+++ spec/react_on_rails/support/shared_examples/base_generator_examples.rb
@@ -51,7 +51,7 @@ shared_examples "base_generator:base" do |options|
   it "removes incompatible sprockets require statements" do
     assert_file("app/assets/javascripts/application.js") do |contents|
       refute_match("//= require_tree .", contents)
-      refute_match(/\/\/= require jquery$/, contents)
+      refute_match(%r{\/\/= require jquery$}, contents)
       assert_match("//= require jquery-ui", contents) if options[:application_js]
       refute_match("//= require jquery_ujs", contents)
     end
