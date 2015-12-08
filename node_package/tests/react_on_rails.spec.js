b4b36fb33f31da5ea630be7a8c5ae5a797aaa8fc
Andy Wang <wangyaodi@gmail.com>
2015-12-08 21:51:46 +0800

    add tests placeholder and script

---
+++ node_package/tests/react_on_rails.spec.js
@@ -0,0 +1,12 @@
+var test = require('tape');
+
+test('test something start from here:', function (t) {
+    t.plan(2);
+
+    t.equal(typeof Date.now, 'function');
+    var start = Date.now();
+
+    setTimeout(function () {
+        t.equal(Date.now() - start, 100);
+    }, 100);
+});
