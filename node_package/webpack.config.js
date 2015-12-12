c852fabe3391a9e1c793d38c2aa2fd11bd273fca
Andy Wang <wangyaodi@gmail.com>
2015-12-12 15:42:50 +0800

    use webpack to export react-on-rails module

---
+++ node_package/webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const fs = require('fs');
+const path = require('path')
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    ReactOnRails: ['babel-polyfill', './src/react_on_rails.js'],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+
+  output: {
+    filename: 'react_on_rails.js',
+    path: path.resolve(__dirname, "./lib"),
+    libraryTarget: 'umd',
+  },
+  externals: [ 'react', 'react-dom', 'react-dom/server'],
+
+  devtool: '#sourcemap',
+
+  plugins: [
+    new webpack.BannerPlugin(
+      'require("source-map-support").install();',
+      { raw: true, entryOnly: false }
+    ),
+  ],
+
+  module: {
+    loaders: [
+      {
+        loader: "babel-loader",
+        include: [
+          path.resolve(__dirname, "./src"),
+        ],
+        test: /\.js$/,
+        query: {
+          plugins: ['transform-runtime'],
+          presets: ['es2015', 'stage-0', 'react'],
+        }
+      },
+    ],
+  },
+};
