4049c0af24fbcb87bce10670d88c5096777fbdfb
Martin Phee <martyphee@gmail.com>
2015-12-28 15:03:03 -0600

    Fix spec.

--- spec/react_on_rails/support/shared_examples/base_generator_examples.rb
+++ spec/react_on_rails/support/shared_examples/base_generator_examples.rb
@@ -95,7 +95,7 @@ shared_examples "base_generator:base" do |options|
       Rails.application.config.assets.paths << Rails.root.join("client", "assets", "stylesheets")
       Rails.application.config.assets.paths << Rails.root.join("client", "assets", "images")
       Rails.application.config.assets.paths << Rails.root.join("client", "assets", "fonts")
-      Rails.application.config.assets.precompile += %w( generated/server-bundle.js )
+      Rails.application.config.assets.precompile += %w( generated/server-bundle.js react_on_rails )
     EXPECTED
     assert_file("config/initializers/assets.rb") { |contents| assert_match(expected, contents) }
   end
