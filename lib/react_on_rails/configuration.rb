5e9a566ba093883ba29353cffd454a7b8ba7fe18
Rob Wise <robert.wise@outlook.com>
2015-12-30 23:09:55 -0500

    Fix example apps WIP

--- lib/react_on_rails/configuration.rb
+++ lib/react_on_rails/configuration.rb
@@ -1,15 +1,9 @@
+require 'react_on_rails/version'
+
 module ReactOnRails
   def self.configure
     yield(configuration)
   end
-
-  # TODO ROB
-  # parse the client/package.json and ensure that either:
-  # 1. version number matches
-  # 2. version number is a relative path (for testing)
-  # Throw error if not.
-  # Allow skipping this check in the configuration in case somebody has a wacky configuration, such
-  # as you don't know where their package.json
 
   def self.configuration
     @configuration ||= Configuration.new(
@@ -23,6 +17,26 @@ module ReactOnRails
       server_renderer_pool_size: 1,
       server_renderer_timeout: 20)
     # TODO ROB: do the version check
+  end
+
+  # TODO ROB
+  # parse the client/package.json and ensure that either:
+  # 1. version number matches
+  # 2. version number is a relative path (for testing)
+  # Throw error if not.
+  # Allow skipping this check in the configuration in case somebody has a wacky configuration, such
+  # as you don't know where their package.json
+
+  def warn_if_module_and_gem_versions_dont_match
+    gem_version = ReactOnRails::VERSION
+    package_json = Rails.root.join("client", "package.json")
+    node_module_version = File.read(package_json).match(/"\n\s*react-on-rails": "(.*)"\s*\n/)[0]
+    processed_node_module_version = node_module_version.delete("^")
+    return unless gem_version != processed_node_module_version
+    puts "**WARNING** ReactOnRails: your version of the ReactOnRails gem (#{gem_version})\n" \
+         "is not equal to your installed version of the react-on-rails node module\n" \
+         "(#{processed_node_module_version}). Using different versions may result in\n" \
+         "compatibility issues."
   end
 
   class Configuration
