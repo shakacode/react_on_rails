c3c69d0eadc1830818c8fb94b9b89e5cd16a4b6e
Rob Wise <robert.wise@outlook.com>
2016-01-03 05:28:06 -0500

    Fix indentation in helloWorldReducer generator template

--- lib/generators/react_on_rails/templates/redux/base/client/app/bundles/HelloWorld/reducers/helloWorldReducer.jsx
+++ lib/generators/react_on_rails/templates/redux/base/client/app/bundles/HelloWorld/reducers/helloWorldReducer.jsx
@@ -10,10 +10,10 @@ export default function helloWorldReducer($$state = $$initialState, action) {
   const { type, name } = action;
 
   switch (type) {
-    case actionTypes.HELLO_WORLD_NAME_UPDATE:
-      return $$state.set('name', name);
+  case actionTypes.HELLO_WORLD_NAME_UPDATE:
+    return $$state.set('name', name);
 
-    default:
-      return $$state;
+  default:
+    return $$state;
   }
 }
