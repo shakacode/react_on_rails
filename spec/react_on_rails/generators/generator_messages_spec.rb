0d467dd2c71a325f0e81c18adc065890aeed53e5
Aaron Van Bokhoven <bokhoven@gmail.com>
2016-01-14 14:09:10 -1000

    Clarify Generator Messages
    - Rename errors to messages because we are no
    longer just printing errors.
    
    - Rename print_errors to print_generator_messages.

--- spec/react_on_rails/generators/generator_messages_spec.rb
+++ spec/react_on_rails/generators/generator_messages_spec.rb
@@ -1,11 +1,11 @@
 describe GeneratorMessages do
   it "has an empty errors array" do
-    expect(GeneratorMessages.errors).to be_empty
+    expect(GeneratorMessages.messages).to be_empty
   end
 
   it "has a method that can add errors" do
     GeneratorMessages.add_error "Test error"
-    expect(GeneratorMessages.errors)
+    expect(GeneratorMessages.messages)
       .to match_array([GeneratorMessages.format_error("Test error")])
   end
 end
