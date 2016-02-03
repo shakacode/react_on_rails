5ad43269a9eee5bcf5cc980938fa80114b5b7a78
Rob Wise <robert.wise@outlook.com>
2016-02-03 16:55:54 -0500

    Improve EnsureAssetsCompiled logic to wait for webpack process to finish
    Because there may be situations where a user is running a webpack
    process in watch mode in the background that will recompile the
    assets, it is possible that if the build time is extremely long,
    the user could start tests before the build completes. In this case,
    EnsureAssetsCompiled would start compiling the assets again even
    though there is already a webpack process doing that.
    
    This commit adds behavior to EnsureAssetsCompiled to check whether
    or not the webpack process is running in the background, and will
    defer the responsibility of compilation to that process, re-checking
    the compiled assets every second until they are up to date.

--- lib/react_on_rails/webpack_assets_status_checker.rb
+++ lib/react_on_rails/webpack_assets_status_checker.rb
@@ -1,4 +1,5 @@
 require "rake"
+require "fileutils"
 
 module ReactOnRails
   class WebpackAssetsStatusChecker
@@ -17,11 +18,11 @@ module ReactOnRails
     private
 
     def all_compiled_assets
-      @all_compiled_assets ||= make_file_list(make_globs(compiled_dirs)).to_ary
+      @all_compiled_assets = make_file_list(make_globs(compiled_dirs)).to_ary
     end
 
     def client_files
-      @client_files ||= make_file_list(make_globs(client_dir)).to_ary
+      @client_files = make_file_list(make_globs(client_dir)).to_ary
     end
 
     def make_globs(dirs)
--- spec/react_on_rails/ensure_assets_compiled_spec.rb
+++ spec/react_on_rails/ensure_assets_compiled_spec.rb
@@ -1,41 +1,62 @@
 require_relative "simplecov_helper"
 require_relative "spec_helper"
 
-class WebpackAssetsCompilerDouble
-  attr_reader :times_ran
-
-  def initialize
-    @times_ran = 0
-  end
-
-  def compile
-    @times_ran += 1
-  end
-end
-
 module ReactOnRails
   describe EnsureAssetsCompiled do
-    let(:compiler) { WebpackAssetsCompilerDouble.new }
-    let(:ensurer) { EnsureAssetsCompiled.new(checker, compiler) }
+    let(:compiler) { double_assets_compiler }
+    let(:ensurer) { EnsureAssetsCompiled.new(assets_checker, compiler, process_checker) }
+    after { ensurer.class.has_been_run = false }
 
     context "when assets are not up to date" do
-      let(:checker) { double_webpack_assets_checker(up_to_date: false) }
+      let(:assets_checker) { double_assets_checker(up_to_date: false) }
 
-      it "compiles the webpack bundles" do
-        expect { ensurer.call }.to change { compiler.times_ran }.from(0).to(1)
+      context "and webpack process is running" do
+        let(:process_checker) { double_process_checker(running: true) }
+
+        it "sleeps until assets are up to date" do
+          expect(compiler).not_to receive(:compile)
+
+          thread = Thread.new { ensurer.call }
+
+          sleep 1
+          allow(assets_checker).to receive(:up_to_date?).and_return(true)
+
+          thread.join
+
+          expect(ensurer.class.has_been_run).to eq(true)
+        end
+      end
+
+      context "and webpack process is NOT running" do
+        let(:process_checker) { double_process_checker(running: false) }
+
+        it "compiles the webpack assets" do
+          expect(compiler).to receive(:compile).once
+          ensurer.call
+        end
       end
     end
 
     context "when assets are up to date" do
-      let(:checker) { double_webpack_assets_checker(up_to_date: true) }
+      let(:assets_checker) { double_assets_checker(up_to_date: true) }
+      let(:process_checker) { double_process_checker(running: false) }
 
-      it "does not compile the webpack bundles if they exist and are up to date" do
-        expect { ensurer.call }.not_to change { compiler.times_ran }
+      it "does nothing" do
+        expect(compiler).not_to receive(:compile)
+        ensurer.call
       end
     end
 
-    def double_webpack_assets_checker(args = {})
+    def double_process_checker(args = {})
+      instance_double(WebpackProcessChecker, running?: args.fetch(:running))
+    end
+
+    def double_assets_checker(args = {})
       instance_double(WebpackAssetsStatusChecker, up_to_date?: args.fetch(:up_to_date))
+    end
+
+    def double_assets_compiler
+      instance_double(WebpackAssetsCompiler, :compile)
     end
   end
 end
