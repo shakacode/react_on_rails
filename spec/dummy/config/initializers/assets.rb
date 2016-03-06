38c4422037eb46e95c355631c1e1ccf3a458ae6c
Justin Gordon <justin.gordon@gmail.com>
2016-03-06 08:20:25 -1000

    Remove unused precompile directive

--- spec/dummy/config/initializers/assets.rb
+++ spec/dummy/config/initializers/assets.rb
@@ -10,10 +10,6 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-unless ENV["DISABLE_TURBOLINKS"].present?
-  Rails.application.config.assets.precompile += %w( application_with_turbolinks.js )
-end
-
 # Add folder with webpack generated assets to assets.paths
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "webpack")
 
