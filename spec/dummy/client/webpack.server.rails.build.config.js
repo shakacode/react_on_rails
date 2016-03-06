aefaf470552dc513484bc752eecd3b3dac391c82
Justin Gordon <justin.gordon@gmail.com>
2016-03-06 00:30:02 -1000

    Setup hot dev mode
    * Added view helpers for assets
    * Converted the spec/dummy app to use css modules
    * added documentation

---
+++ spec/dummy/client/webpack.server.rails.build.config.js
@@ -0,0 +1,56 @@
+// Common webpack configuration for server bundle
+
+const webpack = require('webpack');
+const path = require('path');
+
+const devBuild = process.env.NODE_ENV !== 'production';
+const nodeEnv = devBuild ? 'development' : 'production';
+
+module.exports = {
+
+  // the project dir
+  context: __dirname,
+  entry: [
+    'babel-polyfill',
+    './app/startup/serverRegistration',
+  ],
+  output: {
+    filename: 'server-bundle.js',
+    path: '../app/assets/webpack',
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      libs: path.join(process.cwd(), 'app', 'libs'),
+    },
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(nodeEnv),
+      },
+    }),
+  ],
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.css$/,
+        loaders: [
+          'css/locals?modules&importLoaders=0&localIdentName=[name]__[local]__[hash:base64:5]',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        loaders: [
+          'css/locals?modules&importLoaders=2&localIdentName=[name]__[local]__[hash:base64:5]',
+          'sass',
+          'sass-resources',
+        ],
+      },
+    ],
+  },
+
+  sassResources: ['./app/assets/styles/app-variables.scss'],
+
+};
--- spec/dummy/config/initializers/assets.rb
+++ spec/dummy/config/initializers/assets.rb
@@ -13,3 +13,13 @@ Rails.application.config.assets.version = "1.0"
 unless ENV["DISABLE_TURBOLINKS"].present?
   Rails.application.config.assets.precompile += %w( application_with_turbolinks.js )
 end
+
+# Add folder with webpack generated assets to assets.paths
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "webpack")
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+Rails.application.config.assets.precompile += %w(
+  application_*.*
+  server-bundle.js
+)
