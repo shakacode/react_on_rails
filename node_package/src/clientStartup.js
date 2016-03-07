d73954af709bfa7d925d038411d9cba6c48a779e
SqueezedLight <buchsteinert@gmail.com>
2016-03-07 12:04:39 +0100

    expose reactOnRailsPageLoaded to ReactOnRails

--- node_package/src/clientStartup.js
+++ node_package/src/clientStartup.js
@@ -85,7 +85,7 @@ You should return a React.Component always for the client side entry point.`);
   }
 }
 
-function reactOnRailsPageLoaded() {
+export function reactOnRailsPageLoaded() {
   debugTurbolinks('reactOnRailsPageLoaded');
 
   forEachStore(initializeStore);
@@ -103,7 +103,7 @@ function reactOnRailsPageUnloaded() {
   forEachComponent(unmount);
 }
 
-export default function clientStartup(context) {
+export function clientStartup(context) {
   const document = context.document;
 
   // Check if server rendering
