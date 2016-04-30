0b0e3aa542427a2156eacd346b26ffbd889c6075
jbhatab <jbhatab@gmail.com>
2016-04-29 23:00:28 -0500

    changed asset compliation checks

--- lib/react_on_rails/configuration.rb
+++ lib/react_on_rails/configuration.rb
@@ -58,6 +58,7 @@ module ReactOnRails
       rendering_extension: nil,
       server_render_method: "",
       symlink_non_digested_assets_regex: /\.(png|jpg|jpeg|gif|tiff|woff|ttf|eot|svg)/
+      npm_build_command: "npm run build"
     )
   end
 
@@ -67,7 +68,7 @@ module ReactOnRails
                   :logging_on_server, :server_renderer_pool_size,
                   :server_renderer_timeout, :raise_on_prerender_error,
                   :skip_display_none, :generated_assets_dirs, :generated_assets_dir,
-                  :webpack_generated_files, :rendering_extension,
+                  :webpack_generated_files, :rendering_extension, :npm_build_command,
                   :server_render_method, :symlink_non_digested_assets_regex
 
     def initialize(server_bundle_js_file: nil, prerender: nil, replay_console: nil,
@@ -76,11 +77,12 @@ module ReactOnRails
                    server_renderer_timeout: nil, raise_on_prerender_error: nil,
                    skip_display_none: nil, generated_assets_dirs: nil,
                    generated_assets_dir: nil, webpack_generated_files: nil,
-                   rendering_extension: nil, server_render_method: nil,
-                   symlink_non_digested_assets_regex: nil)
+                   rendering_extension: nil, npm_build_command: nil,
+                   server_render_method: nil, symlink_non_digested_assets_regex: nil)
       self.server_bundle_js_file = server_bundle_js_file
       self.generated_assets_dirs = generated_assets_dirs
       self.generated_assets_dir = generated_assets_dir
+      self.npm_build_command = npm_build_command
 
       self.prerender = prerender
       self.replay_console = replay_console
