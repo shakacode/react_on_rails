3c4cdca8d6a228e3a2be5dbffaa52b28f872f798
Anton Maminov <anton.linux@gmail.com>
2016-05-31 15:15:11 +0300

    Ignore untracked files in check uncommited code in a project

--- lib/react_on_rails/git_utils.rb
+++ lib/react_on_rails/git_utils.rb
@@ -2,7 +2,7 @@ module ReactOnRails
   module GitUtils
     def self.uncommitted_changes?(message_handler)
       return false if ENV["COVERAGE"]
-      status = `git status --porcelain`
+      status = `git status --porcelain`.lines.reject{ |line| line.start_with?('??') }
       return false if status.empty?
       error = "You have uncommitted code. Please commit or stash your changes before continuing"
       message_handler.add_error(error)
