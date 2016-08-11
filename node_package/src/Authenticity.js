e02676e147b510c349fdfdc5fa45b12d7a65779a
Sergey Tarasov <dzirtusss@gmail.com>
2016-08-11 11:43:21 +0300

    added CSRF helpers authenticityToken and authenticityHeaders

---
+++ node_package/src/Authenticity.js
@@ -0,0 +1,14 @@
+export default {
+
+  authenticityToken() {
+    const token = document.querySelector('meta[name="csrf-token"]');
+    return token ? token.content : null;
+  },
+
+  authenticityHeaders(otherHeaders = {}) {
+    return Object.assign(otherHeaders, {
+      'X-CSRF-Token': this.authenticityToken(),
+      'X-Requested-With': 'XMLHttpRequest'
+    });
+  },
+};
