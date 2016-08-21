38cc105048263b2cdbb8d0d98129b27826a3d317
AlexKVal <alexkval@gmail.com>
2016-08-21 19:04:06 +0300

    Enable flowtype for Authenticity.js
    src/Authenticity.js
    without {content?: string} it warns about
    > property `content` Property not found in HTMLElement

--- node_package/src/context.js
+++ node_package/src/context.js
@@ -1,7 +1,8 @@
+// @flow
+
 /**
  * Get the context, be it window or global
  * @returns {boolean|Window|*|context}
- * @flow
  */
 export default function context() {
   return ((typeof window !== 'undefined') && window) ||
