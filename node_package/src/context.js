22c23a6a28d9875b59e0905248ebf589a3971cdb
AlexKVal <alexkval@gmail.com>
2016-08-21 13:15:31 +0300

    Add 'flowtype' checking to 'node_package' folder

--- node_package/src/context.js
+++ node_package/src/context.js
@@ -1,6 +1,7 @@
 /**
  * Get the context, be it window or global
  * @returns {boolean|Window|*|context}
+ * @flow
  */
 export default function context() {
   return ((typeof window !== 'undefined') && window) ||
