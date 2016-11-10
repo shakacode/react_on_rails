8f770159d80ed735e11cabe313b779bae34f5c45
Ben Wheeler <wheeler.benjamin@gmail.com>
2016-11-10 16:16:57 -0500

    handle error where react-on-rails package entry missing

--- lib/react_on_rails/version_checker.rb
+++ lib/react_on_rails/version_checker.rb
@@ -58,7 +58,13 @@ module ReactOnRails
       end
 
       def raw
-        JSON.parse(package_json_contents)["dependencies"]["react-on-rails"]
+        parsed_package_contents = JSON.parse(package_json_contents)
+        if parsed_package_contents.key?("dependencies") &&
+           parsed_package_contents["dependencies"].key?("react-on-rails")
+          parsed_package_contents["dependencies"]["react-on-rails"]
+        else
+          raise "no 'react-on-rails' entry in package.json dependencies"
+        end
       end
 
       def relative_path?
