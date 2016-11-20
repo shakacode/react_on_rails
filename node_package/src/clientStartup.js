1918345809869ee7d1a792bd894e52e842104243
Volkan Unsal <spocksplanet@gmail.com>
2016-11-19 22:58:33 -0500

    Fixes #610 (#611) Turbolinks should be before-cache
    not before-render

--- node_package/src/clientStartup.js
+++ node_package/src/clientStartup.js
@@ -158,10 +158,10 @@ export function clientStartup(context) {
       reactOnRailsPageLoaded();
     } else if (turbolinksVersion5()) {
       debugTurbolinks(
-        'USING TURBOLINKS 5: document added event listeners turbolinks:before-render and ' +
+        'USING TURBOLINKS 5: document added event listeners turbolinks:before-cache and ' +
         'turbolinks:load.'
       );
-      document.addEventListener('turbolinks:before-render', reactOnRailsPageUnloaded);
+      document.addEventListener('turbolinks:before-cache', reactOnRailsPageUnloaded);
       document.addEventListener('turbolinks:load', reactOnRailsPageLoaded);
     } else {
       debugTurbolinks(
