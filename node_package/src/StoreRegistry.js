843c43dfc683acc81b0e7c34ee1df68c17eff848
Sean Kenny <sean.kenny@hotmail.com>
2016-12-01 00:26:11 +0000

    Slight improvement to the 'no hydrated stores' error (#605)
    * Slight improvement to the 'no hydrated stores' error
    This should make it more clear that rendering
    `redux_store_hydration_data` is required.
    This should more cleanly differentiate the two cases
    * Change hydrated stores error to be a list of actions

--- node_package/src/StoreRegistry.js
+++ node_package/src/StoreRegistry.js
@@ -39,10 +39,12 @@ export default {
     const storeKeys = Array.from(hydratedStores.keys()).join(', ');
 
     if (storeKeys.length === 0) {
-      const msg = 'There are no stores hydrated and you are requesting the store ' +
-        `${name}. This can happen if you are server rendering and you do not call ` +
-        "redux_store near the top of your controller action's view (not the layout) " +
-        'and before any call to react_component.';
+      const msg =
+`There are no stores hydrated and you are requesting the store ${name}.
+This can happen if you are server rendering and either:
+1. You do not call redux_store near the top of your controller action's view (not the layout)
+   and before any call to react_component.
+2. You do not render redux_store_hydration_data anywhere on your page.`;
       throw new Error(msg);
     }
 
