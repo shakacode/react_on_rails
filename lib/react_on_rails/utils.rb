9d77c9da66f935a0df5b224c103f3c36cf6dc799
GeorgeGorbanev <GeorgeGorbanev@gmail.com>
2017-01-12 19:49:00 +0300

    utils

--- lib/react_on_rails/utils.rb
+++ lib/react_on_rails/utils.rb
@@ -20,13 +20,27 @@ module ReactOnRails
                 ReactOnRails.configuration.server_bundle_js_file)
     end
 
-    def self.running_on_windows?
-      (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
-    end
-
     module Required
       def required(arg_name)
         raise ArgumentError, "#{arg_name} is required"
+      end
+    end
+
+    module OS
+      def OS.windows?
+        (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
+      end
+
+      def OS.mac?
+        (/darwin/ =~ RUBY_PLATFORM) != nil
+      end
+
+      def OS.unix?
+        !OS.windows?
+      end
+
+      def OS.linux?
+        OS.unix? and not OS.mac?
       end
     end
   end
