58a4c4557f3bea3988883cf0255d901985ad4706
Alexey Mahotkin <squadette@gmail.com>
2017-01-25 15:00:02 +0100

    Yarn migration, pt. III: update generators

--- lib/generators/react_on_rails/base_generator.rb
+++ lib/generators/react_on_rails/base_generator.rb
@@ -138,11 +138,11 @@ Rails.application.config.assets.paths << Rails.root.join("app", "assets", "webpa
 
           What to do next:
 
-            - Ensure your bundle and npm are up to date.
+            - Ensure your bundle and yarn installs of dependencies are up to date.
 
-                bundle && npm i
+                bundle && yarn
 
-            - Run the foreman command to start the rails server and run webpack in watch mode.
+            - Run the foreman command to load the rails server and webpack process
 
                 foreman start -f Procfile.dev
 
