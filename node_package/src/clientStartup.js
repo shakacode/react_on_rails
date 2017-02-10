052b662c97d0dba79f16a10445149140145f2b22
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2017-02-10 16:52:55 +1000

    linting fixes

--- node_package/src/clientStartup.js
+++ node_package/src/clientStartup.js
@@ -182,7 +182,8 @@ export function clientStartup(context) {
     if (turbolinksInstalled() && turbolinksSupported()) {
       if (turbolinksVersion5()) {
         debugTurbolinks(
-          `USING TURBOLINKS 5: document added event listeners turbolinks:before-render and turbolinks:load.`);
+          'USING TURBOLINKS 5: document added event listeners ' +
+          'turbolinks:before-render and turbolinks:load.');
         document.addEventListener('turbolinks:before-render', reactOnRailsPageUnloaded);
         document.addEventListener('turbolinks:load', reactOnRailsPageLoaded);
       } else {
