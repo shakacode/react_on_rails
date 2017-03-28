cb3c9d076144596918f2195c9580c5dcb707322c
cheremukhin23 <dcheremukhin1@gmail.com>
2017-03-28 23:19:05 +0300

    Improve jsonEl parsing to handle props as already converted from JSON to a string

--- node_package/src/clientStartup.js
+++ node_package/src/clientStartup.js
@@ -97,7 +97,11 @@ DELEGATING TO RENDERER ${name} for dom node with id: ${domNodeId} with props, ra
  */
 function render(el, railsContext) {
   const context = findContext();
-  const jsonEl = JSON.parse(el.textContent);
+  var jsonEl = JSON.parse(el.textContent, (key, value) =>
+    key === 'props' && typeof(value) === 'string'
+      ? JSON.parse(value)
+      : value
+  );
   const name = jsonEl.component_name;
   const domNodeId = jsonEl.dom_id;
   const props = jsonEl.props;
