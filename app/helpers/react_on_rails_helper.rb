6df808e41bd004094541a81c22723796e5839826
Edmund Li <edmund.xz.lee@gmail.com>
2017-04-20 19:47:17 -0700

    use json string

--- app/helpers/react_on_rails_helper.rb
+++ app/helpers/react_on_rails_helper.rb
@@ -306,7 +306,7 @@ module ReactOnRailsHelper
     # If to_json is called on a String, then the quotes are escaped.
     json_value = hash_or_string.is_a?(String) ? hash_or_string : hash_or_string.to_json
 
-    ERB::Util.json_escape(json_value)
+    # ERB::Util.json_escape(json_value)
     # end
   end
 
