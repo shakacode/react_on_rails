7f270545ed1aa5ede83fbf7b1c4bc7ff3773295f
Daniel Szatmari <daniel.kaizencodes@gmail.com>
2017-04-25 15:31:38 +0200

    fix dummy bundle generation

--- spec/dummy/client/webpack.server.rails.build.config.js
+++ spec/dummy/client/webpack.server.rails.build.config.js
@@ -7,20 +7,11 @@ const { imageLoaderRules } = require('./webpack.common');
 const webpackCommon = require('./webpack.common');
 const { assetLoaderRules } = webpackCommon;
 
-const ManifestPlugin = require('webpack-manifest-plugin');
 const webpackConfigLoader = require('react-on-rails').default.webpackConfigLoader;
 const { env, paths, publicPath } = webpackConfigLoader(resolve('..', 'config', 'webpack'));
-const manifestPath = resolve('..', paths.output, paths.assets, paths.manifest);
 
 const devBuild = process.env.NODE_ENV !== 'production';
 const nodeEnv = devBuild ? 'development' : 'production';
-
-let sharedManifest = {};
-try {
-  sharedManifest = require(manifestPath);
-} catch (ex) {
-  console.info('No manifest found. It will be created at:', manifestPath);
-}
 
 module.exports = {
 
@@ -37,20 +28,14 @@ module.exports = {
     extensions: ['.js', '.jsx'],
     alias: {
       images: join(process.cwd(), 'app', 'assets', 'images'),
-      // 'react-on-rails': resolve(__dirname, '..', '..', '..'),
     },
   },
+
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(nodeEnv),
       },
-    }),
-    new ManifestPlugin({
-      fileName: paths.manifest,
-      publicPath,
-      writeToFileEmit: true,
-      cache: sharedManifest,
     }),
   ],
   module: {
