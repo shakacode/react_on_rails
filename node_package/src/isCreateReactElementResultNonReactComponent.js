f7f0757425f331596a497a3cae19c1b881bc6215
Denis Udovenko <denis.e.udovenko@gmail.com>
2017-05-01 12:04:23 +1100

    Node package cleanup

--- node_package/src/isCreateReactElementResultNonReactComponent.js
+++
@@ -1,6 +0,0 @@
-export default function isResultNonReactComponent(reactElementOrRouterResult) {
-  return !!(
-    reactElementOrRouterResult.renderedHtml ||
-    reactElementOrRouterResult.redirectLocation ||
-    reactElementOrRouterResult.error);
-}
