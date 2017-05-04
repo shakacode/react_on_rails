6e684c8530b66f7c2b6271f6ad7b8cc57fcb6c67
Denis Udovenko <denis.e.udovenko@gmail.com>
2017-05-04 13:39:23 +1100

    Move all worker modules to "worker" dir

--- node_package/src/worker.js
+++ node_package/src/worker.js
@@ -1,9 +1,9 @@
 const cluster = require('cluster');
 const express = require('express');
 const bodyParser = require('body-parser');
-const { runInVM } = require('./vm');
-const configBuilder = require('./configBuilder');
-const bundleWatcher = require('./bundleWatcher');
+const { runInVM } = require('./worker/vm');
+const configBuilder = require('./worker/configBuilder');
+const bundleWatcher = require('./worker/bundleWatcher');
 
 exports.run = function run() {
   const { bundlePath, bundleFileName, port } = configBuilder();
