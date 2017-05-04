04f7078555a626ee572c4df84b4e7b9e7d6978e8
Denis Udovenko <denis.e.udovenko@gmail.com>
2017-05-04 13:50:20 +1100

    Moved master process logic to separate master module

--- react_on_rails_pro/node_package/src/ReactOnRailsRenderer.js
+++ react_on_rails_pro/node_package/src/ReactOnRailsRenderer.js
@@ -1,21 +1,9 @@
 const cluster = require('cluster');
+const master = require('./master');
 const worker = require('./worker');
 
 if (cluster.isMaster) {
-  // Count available CPUs for worker processes:
-  const workerCpuCount = require('os').cpus().length - 1 || 1;
-
-  // Create a worker for each CPU except one that used for master process:
-  for (let i = 0; i < workerCpuCount; i += 1) {
-    cluster.fork();
-  }
-
-  // Listen for dying workers:
-  cluster.on('exit', (worker) => {
-    // Replace the dead worker:
-    console.log('Worker %d died :(', worker.id);
-    cluster.fork();
-  });
+  master.run();
 } else {
   worker.run();
 }
