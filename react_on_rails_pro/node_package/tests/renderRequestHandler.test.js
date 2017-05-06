e509179ceb75ddd96fbd9fe5b0ef15d2130aedd0
Denis Udovenko <denis.e.udovenko@gmail.com>
2017-05-06 17:10:54 +1100

    Add more tests for renderRequestHandler

--- react_on_rails_pro/node_package/tests/renderRequestHandler.test.js
+++ react_on_rails_pro/node_package/tests/renderRequestHandler.test.js
@@ -1,6 +1,8 @@
 const test = require('tape');
 const path = require('path');
+const fs = require('fs');
 const { buildConfig } = require('../src/worker/configBuilder');
+const { getBundleUpdateTimeUtc } = require('../src/worker/vm');
 const renderRequestHandler = require('../src/worker/renderRequestHandler');
 
 function setConfig() {
@@ -9,13 +11,16 @@ function setConfig() {
   });
 }
 
-function getUploadedBundlePath() {
+function cleanUploadedBundle() {
+  fs.unlink(path.resolve(__dirname, './tmp/bundle.js'));
+}
+
+function getTmpUploadedBundlePath() {
   return path.resolve(__dirname, './fixtures/bundle.js');
 }
 
 test('If gem has posted updated bundle', (assert) => {
-  assert.plan(1);
-
+  assert.plan(2);
   setConfig();
 
   const req = {
@@ -24,10 +29,42 @@ test('If gem has posted updated bundle', (assert) => {
     },
     files: {
       bundle: {
-        file: getUploadedBundlePath(),
+        file: getTmpUploadedBundlePath(),
       },
     },
   };
 
-  assert.deepEqual(renderRequestHandler(req), { status: 200, data: { renderedHtml: 'Dummy Object' } });
+  const result = renderRequestHandler(req);
+
+  assert.deepEqual(
+    result,
+    { status: 200, data: { renderedHtml: 'Dummy Object' } },
+    'renderRequestHandler returns status 200 and correct rendered renderedHtmls');
+  assert.equal(
+    getBundleUpdateTimeUtc(),
+    +(fs.statSync(path.resolve(__dirname, './tmp/bundle.js')).mtime),
+    'getBundleUpdateTimeUtc() should return last modification time of bundle loaded to VM');
+});
+
+test('If bundle was not uploaded yet', (assert) => {
+  assert.plan(1);
+  const updateBundleTimestamp = +(fs.statSync(path.resolve(__dirname, './tmp/bundle.js')).mtime) + 1;
+
+  cleanUploadedBundle();
+  setConfig();
+
+  const req = {
+    files: {},
+    body: {
+      renderingRequest: 'ReactOnRails.dummy',
+      bundleUpdateTimeUtc: updateBundleTimestamp,
+    },
+  };
+
+  const result = renderRequestHandler(req);
+
+  assert.deepEqual(
+    result,
+    { status: 410, data: 'No bundle uploaded' },
+    'renderRequestHandler returns status 410 with "No bundle uploaded"');
 });
