68dc38079ea0fbf4ce45594ef08801566e47bbc2
Daniel Szatmari <daniel.kaizencodes@gmail.com>
2017-05-11 11:21:59 +0200

    use webpack-merge (#838)
    - fix hot config bundle name

--- spec/dummy/client/webpack.client.rails.build.config.js
+++ spec/dummy/client/webpack.client.rails.build.config.js
@@ -5,6 +5,7 @@
 /* eslint-disable comma-dangle */
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const merge = require('webpack-merge');
 const config = require('./webpack.client.base.config');
 const { resolve } = require('path');
 const webpackConfigLoader = require('react-on-rails/webpackConfigLoader');
@@ -13,96 +14,6 @@ const { paths, publicPath } = webpackConfigLoader(configPath);
 
 const devBuild = process.env.NODE_ENV !== 'production';
 
-config.output = {
-  filename: '[name].js',
-  path: resolve('..', paths.output, paths.assets),
-  publicPath,
-},
-
-// See webpack.client.base.config for adding modules common to both the webpack dev server and rails
-
-config.module.rules.push(
-  {
-    test: /\.jsx?$/,
-    use: 'babel-loader',
-    exclude: /node_modules/,
-  },
-  {
-    test: /\.css$/,
-    loader: ExtractTextPlugin.extract({
-      fallback: 'style-loader',
-      use: [
-        {
-          loader: 'css-loader',
-          options: {
-            minimize: true,
-            modules: true,
-            importLoaders: 1,
-            localIdentName: '[name]__[local]__[hash:base64:5]',
-          },
-        },
-        'postcss-loader',
-      ],
-    }),
-  },
-  {
-    test: /\.scss$/,
-    use: ExtractTextPlugin.extract({
-      fallback: 'style-loader',
-      loader: [
-        {
-          loader: 'css-loader',
-          options: {
-            minimize: true,
-            modules: true,
-            importLoaders: 3,
-            localIdentName: '[name]__[local]__[hash:base64:5]',
-          },
-        },
-        {
-          loader: 'postcss-loader',
-          options: {
-            plugins: 'autoprefixer'
-          }
-        },
-        'sass-loader',
-        {
-          loader: 'sass-resources-loader',
-          options: {
-            resources: './app/assets/styles/app-variables.scss'
-          },
-        }
-      ],
-    }),
-  },
-  {
-    test: require.resolve('react'),
-    use: {
-      loader: 'imports-loader',
-      options: {
-        shim: 'es5-shim/es5-shim',
-        sham: 'es5-shim/es5-sham',
-      }
-    }
-  },
-  {
-    test: require.resolve('jquery-ujs'),
-    use: {
-      loader: 'imports-loader',
-      options: {
-        jQuery: 'jquery',
-      }
-    }
-  }
-);
-
-config.plugins.push(
-  new ExtractTextPlugin({
-    filename: '[name].css',
-    allChunks: true
-  })
-);
-
 if (devBuild) {
   console.log('Webpack dev build for Rails'); // eslint-disable-line no-console
   config.devtool = 'eval-source-map';
@@ -110,4 +21,97 @@ if (devBuild) {
   console.log('Webpack production build for Rails'); // eslint-disable-line no-console
 }
 
-module.exports = config;
+module.exports = merge(config, {
+
+  output: {
+    filename: '[name].js',
+    path: resolve('..', paths.output, paths.assets),
+    publicPath,
+  },
+
+  // See webpack.client.base.config for adding modules common to both the webpack dev server and rails
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true,
+                modules: true,
+                importLoaders: 1,
+                localIdentName: '[name]__[local]__[hash:base64:5]',
+              },
+            },
+            'postcss-loader',
+          ],
+        }),
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          loader: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true,
+                modules: true,
+                importLoaders: 3,
+                localIdentName: '[name]__[local]__[hash:base64:5]',
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: 'autoprefixer'
+              }
+            },
+            'sass-loader',
+            {
+              loader: 'sass-resources-loader',
+              options: {
+                resources: './app/assets/styles/app-variables.scss'
+              },
+            }
+          ],
+        }),
+      },
+      {
+        test: require.resolve('react'),
+        use: {
+          loader: 'imports-loader',
+          options: {
+            shim: 'es5-shim/es5-shim',
+            sham: 'es5-shim/es5-sham',
+          }
+        }
+      },
+      {
+        test: require.resolve('jquery-ujs'),
+        use: {
+          loader: 'imports-loader',
+          options: {
+            jQuery: 'jquery',
+          }
+        }
+      }
+    ],
+  },
+
+  plugins: [
+    new ExtractTextPlugin({
+      filename: '[name].css',
+      allChunks: true
+    }),
+  ],
+});
--- spec/dummy/client/webpack.client.rails.hot.config.js
+++ spec/dummy/client/webpack.client.rails.hot.config.js
@@ -6,112 +6,117 @@
 
 const { resolve } = require('path');
 const webpack = require('webpack');
-
+const merge = require('webpack-merge');
 const config = require('./webpack.client.base.config');
 const webpackConfigLoader = require('react-on-rails/webpackConfigLoader');
 const configPath = resolve('..', 'config', 'webpack');
 const { devServer, paths, publicPath } = webpackConfigLoader(configPath);
 
-config.entry.app.push(
-  `webpack-dev-server/client?${devServer.host}:${devServer.port}`,
-  'webpack/hot/only-dev-server'
-);
+module.exports = merge(config, {
 
-config.output = {
-  filename: '[name].js',
-  path: resolve('..', paths.output, paths.assets),
-  publicPath,
-};
+  config.devtool: 'eval-source-map',
 
-config.module.rules.push(
-  {
-    test: /\.jsx?$/,
-    loader: 'babel-loader',
-    exclude: /node_modules/,
-    query: {
-      plugins: [
-        [
-          'react-transform',
-          {
-            transforms: [
-              {
-                transform: 'react-transform-hmr',
-                imports: ['react'],
-                locals: ['module'],
-              },
-            ],
-          },
-        ],
-      ],
-    },
-  },
-  {
-    test: /\.css$/,
-    use: [
-      'style-loader',
-      {
-        loader: 'css-loader',
-        options: {
-          modules: true,
-          importLoaders: 0,
-          localIdentName: '[name]__[local]__[hash:base64:5]'
-        }
-      },
-      {
-        loader: 'postcss-loader',
-        options: {
-          plugins: 'autoprefixer'
-        }
-      }
-    ]
-  },
-  {
-    test: /\.scss$/,
-    use: [
-      'style-loader',
-      {
-        loader: 'css-loader',
-        options: {
-          modules: true,
-          importLoaders: 3,
-          localIdentName: '[name]__[local]__[hash:base64:5]',
-        }
-      },
-      {
-        loader: 'postcss-loader',
-        options: {
-          plugins: 'autoprefixer'
-        }
-      },
-      {
-        loader: 'sass-loader'
-      },
-      {
-        loader: 'sass-resources-loader',
-        options: {
-          resources: './app/assets/styles/app-variables.scss'
-        },
-      }
+  entry: {
+    'app-bundle': [
+      `webpack-dev-server/client?${devServer.host}:${devServer.port}`,
+      'webpack/hot/only-dev-server'
     ],
   },
-  {
-    test: require.resolve('jquery-ujs'),
-    use: {
-      loader: 'imports-loader',
-      options: {
-        jQuery: 'jquery',
-      }
-    }
-  }
-);
 
-config.plugins.push(
-  new webpack.HotModuleReplacementPlugin(),
-  new webpack.NoEmitOnErrorsPlugin()
-);
+  output: {
+    filename: '[name].js',
+    path: resolve('..', paths.output, paths.assets),
+    publicPath,
+  },
 
-config.devtool = 'eval-source-map';
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          plugins: [
+            [
+              'react-transform',
+              {
+                transforms: [
+                  {
+                    transform: 'react-transform-hmr',
+                    imports: ['react'],
+                    locals: ['module'],
+                  },
+                ],
+              },
+            ],
+          ],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 0,
+              localIdentName: '[name]__[local]__[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: 'autoprefixer'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 3,
+              localIdentName: '[name]__[local]__[hash:base64:5]',
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: 'autoprefixer'
+            }
+          },
+          {
+            loader: 'sass-loader'
+          },
+          {
+            loader: 'sass-resources-loader',
+            options: {
+              resources: './app/assets/styles/app-variables.scss'
+            },
+          }
+        ],
+      },
+      {
+        test: require.resolve('jquery-ujs'),
+        use: {
+          loader: 'imports-loader',
+          options: {
+            jQuery: 'jquery',
+          }
+        }
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+});
 
 console.log('Webpack HOT dev build for Rails'); // eslint-disable-line no-console
-
-module.exports = config;
