5a7a57db899d11130ebdaed089671ccd27959b99
Denis Udovenko <denis.e.udovenko@gmail.com>
2017-05-12 16:21:53 +1100

    Create examples index and move initial example under separate route for dummy app

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root "pages#index"
-
-  # get "client_side_hello_world" => "pages#client_side_hello_world"
+  get "shared_redux_store" => "pages#shared_redux_store"
 end
