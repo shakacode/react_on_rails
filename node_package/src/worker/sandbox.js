533766894c8544fab8771506ff62160bf4637da1
Denis Udovenko <denis.e.udovenko@gmail.com>
2017-05-12 13:06:30 +1100

    Move console.history polyfill to separate module and following refactoring

--- node_package/src/worker/sandbox.js
+++ node_package/src/worker/sandbox.js
@@ -6,28 +6,7 @@
 const fs = require('fs');
 const SandboxedModule = require('sandboxed-module');
 const cluster = require('cluster');
-
-// Add history to console:
-require('console.history');
-
-/**
- * Overrirde _collect method of console.history.
- * See https://github.com/lesander/console.history/blob/master/console-history.js for details.
- */
-// eslint-disable-next-line no-underscore-dangle
-console._collect = (type, args) => {
-  // Act normal, and just pass all original arguments to the origial console function:
-  // eslint-disable-next-line prefer-spread
-  console[`_${type}`].apply(console, args);
-
-  // Build console history entry in react_on_rails format:
-  const argArray = Array.prototype.slice.call(args);
-  if (argArray.length > 0) {
-    argArray[0] = `[SERVER] ${argArray[0]}`;
-  }
-
-  console.history.push({ level: 'log', arguments: argArray });
-};
+const { clearConsoleHistory } = require('./consoleHistory');
 
 let vm;
 let bundleUpdateTimeUtc;
@@ -36,10 +15,10 @@ let bundleUpdateTimeUtc;
  *
  */
 exports.buildVM = function buildVMNew(filePath) {
-  bundleUpdateTimeUtc = +(fs.statSync(filePath).mtime);
-
   fs.appendFileSync(filePath, '; exports.run = function(code) { return eval(code) };');
   vm = SandboxedModule.load(filePath);
+
+  bundleUpdateTimeUtc = +(fs.statSync(filePath).mtime);
 
   if (!cluster.isMaster) console.log(`Built VM for worker #${cluster.worker.id}`);
   console.log('Required objects now in VM sandbox context:', vm.exports.run('ReactOnRails') !== undefined);
@@ -58,7 +37,7 @@ exports.getBundleUpdateTimeUtc = function getBundleUpdateTimeUtc() {
  *
  */
 exports.runInVM = function runInVM(code) {
-  console.history = [];
+  clearConsoleHistory();
   const result = vm.exports.run(code);
   return result;
 };
