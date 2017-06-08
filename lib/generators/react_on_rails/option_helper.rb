441ad40c9c7dfa42596ea5dc9d1f3063c373b46f
Dmitry Maksyoma <ledestin@gmail.com>
2017-06-08 16:41:17 +1200

    Generators: update #dst_filename to work with camel case

--- lib/generators/react_on_rails/option_helper.rb
+++ lib/generators/react_on_rails/option_helper.rb
@@ -10,7 +10,9 @@ module OptionHelper
   end
 
   def dst_filename(src_filename)
-    src_filename.gsub /hello_world/, example_page_path
+    src_filename.gsub(/hello_world/, example_page_path)
+                .gsub(/HelloWorld/, example_page_name)
+                .gsub(/helloWorld/, example_page_name.camelcase(:lower))
   end
 
   module ClassMethods
