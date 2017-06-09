d9b56ab577fa4ef872adfe6bbc2802866a9746c4
Dmitry Maksyoma <ledestin@gmail.com>
2017-06-09 16:38:04 +1200

    Generators: convert the rest of templates to heed --example-page-name

--- lib/generators/react_on_rails/templates/redux/base/client/app/bundles/HelloWorld/actions/helloWorldActionCreators.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-
-import { HELLO_WORLD_NAME_UPDATE } from '../constants/helloWorldConstants';
-
-export const updateName = (text) => ({
-  type: HELLO_WORLD_NAME_UPDATE,
-  text,
-});
--- lib/generators/react_on_rails/templates/redux/base/client/app/bundles/HelloWorld/store/helloWorldStore.jsx
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux';
-import helloWorldReducer from '../reducers/helloWorldReducer';
-
-const configureStore = (railsProps) => (
-  createStore(helloWorldReducer, railsProps)
-);
-
-export default configureStore;
