b0bb4ef41ff5243b8b4efc5ca92cffd72c4eb9c3
Jason Blalock <jasonblalock@users.noreply.github.com>
2017-07-04 21:17:38 -0700

    Update #uncommitted_changes? to handle missing Git
    - Fix for GitUtils.uncommitted_changes? throwing an error when called in
      an environment that does not have git installed
    - Add tests for GitUtils.uncommitted_changes? scenariors
    - Fixes #888

--- lib/react_on_rails/git_utils.rb
+++ lib/react_on_rails/git_utils.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
+require "English"
+
 module ReactOnRails
   module GitUtils
     def self.uncommitted_changes?(message_handler)
       return false if ENV["COVERAGE"] == "true"
       status = `git status --porcelain`
-      return false if status.empty?
+      return false if $CHILD_STATUS.success? && status.empty?
       error = "You have uncommitted code. Please commit or stash your changes before continuing"
       message_handler.add_error(error)
       true
