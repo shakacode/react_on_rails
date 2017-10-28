shakacode/react_on_rails

commit 820ac3cdac2cf4a1dcd85861d6fe821f262a2119

change i18n directory check (#899)
Source of the issue is that for a directory object, blank? returns true
if the directory is empty.

* change i18n directory check
* added test for i18n_dir = '' in addition to i18n_dir = nil, removed whitespaces, updated changelog
*  if somebody had used "blank" ("") before and they upgrade to this fix, then they probably will be surprised.

lib/react_on_rails/locales_to_js.rb
@@ -5,7 +5,10 @@ require "erb"
 module ReactOnRails
   class LocalesToJs
     def initialize
-      return if i18n_dir.blank?
+      if !i18n_dir.nil? && !File.directory?(i18n_dir)
+        raise "config.i18n_dir is set and it is not a directory. Did you set config.i18n_dir in the react_on_rails initializer?"
+      end
+      return if i18n_yml_dir.nil?
       return unless obsolete?
       @translations, @defaults = generate_translations
       convert

spec/react_on_rails/locales_to_js_spec.rb
@@ -9,6 +9,13 @@ module ReactOnRails
     let(:translations_path) { "#{i18n_dir}/translations.js" }
     let(:default_path) { "#{i18n_dir}/default.js" }
 
+    it "with i18n_dir set to ''" do
+      ReactOnRails.configure do |config|
+        config.i18n_dir = ''
+      end
+      expect { ReactOnRails::LocalesToJs.new }.to raise_error(/config.i18n_dir is set and it is not a directory. Did you set config.i18n_dir in the react_on_rails initializer?/)
+    end
+    
     shared_examples "locale to js" do
       context "with obsolete js files" do
         before do

commit 882cc5dc1c2a178a17430f6f2dcab28f5d3509f5

fixing configuration checks (#967)
fixed and improved configuration checks broken by #899

lib/react_on_rails/configuration.rb
@@ -21,7 +21,7 @@ module ReactOnRails
   end
 
   def self.check_i18n_directory_exists
-    return if @configuration.i18n_dir.blank?
+    return if @configuration.i18n_dir.nil?
     return if Dir.exist?(@configuration.i18n_dir)
 
     raise "Error configuring /config/react_on_rails.rb: invalid value for `config.i18n_dir`. "\
@@ -30,7 +30,7 @@ module ReactOnRails
   end
 
   def self.check_i18n_yml_directory_exists
-    return if @configuration.i18n_yml_dir.blank?
+    return if @configuration.i18n_yml_dir.nil?
     return if Dir.exist?(@configuration.i18n_yml_dir)
 
     raise "Error configuring /config/react_on_rails.rb: invalid value for `config.i18n_yml_dir`. "\
@@ -105,8 +105,6 @@ module ReactOnRails
       server_render_method: "ExecJS",
       symlink_non_digested_assets_regex: nil,
       build_test_command: "",
-      i18n_dir: "",
-      i18n_yml_dir: "",
       build_production_command: ""
     )
   end

lib/react_on_rails/locales_to_js.rb
@@ -5,10 +5,7 @@ require "erb"
 module ReactOnRails
   class LocalesToJs
     def initialize
-      if !i18n_dir.nil? && !File.directory?(i18n_dir)
-        raise "config.i18n_dir is set and it is not a directory. Did you set config.i18n_dir in the react_on_rails initializer?"
-      end
-      return if i18n_yml_dir.nil?
+      return if i18n_dir.nil?
       return unless obsolete?
       @translations, @defaults = generate_translations
       convert

spec/react_on_rails/configuration_spec.rb
@@ -4,42 +4,71 @@ require_relative "spec_helper"
 
 module ReactOnRails
   RSpec.describe Configuration do
-    it "raises if the i18n directory does not exist" do
-      junk_name = "/XXXX/junkXXXX"
-      expect do
-        ReactOnRails.configure do |config|
-          config.i18n_dir = junk_name
-        end
-      end.to raise_error(/#{junk_name}/)
+    let(:existing_path) { Pathname.new(Dir.mktmpdir) }
+    let(:not_existing_path) { "/path/to/#{SecureRandom.hex(4)}" }
+
+    describe ".i18n_dir" do
+      let(:i18n_dir) { existing_path }
+
+      after do
+        ReactOnRails.configure { |config| config.i18n_dir = nil }
+      end
+
+      it "passes if directory exists" do
+        expect do
+          ReactOnRails.configure do |config|
+            config.i18n_dir = i18n_dir
+          end
+        end.not_to raise_error
+      end
+
+      it "fails with empty string value" do
+        expect do
+          ReactOnRails.configure do |config|
+            config.i18n_dir = ""
+          end
+        end.to raise_error(RuntimeError, /invalid value for `config\.i18n_dir`/)
+      end
+
+      it "fails with not existing directory" do
+        expect do
+          ReactOnRails.configure do |config|
+            config.i18n_dir = not_existing_path
+          end
+        end.to raise_error(RuntimeError, /invalid value for `config\.i18n_dir`/)
+      end
     end
 
-    it "does not raises if the i18n directory does exist" do
-      dir = __dir__
-      expect do
-        ReactOnRails.configure do |config|
-          config.i18n_dir = dir
-        end
-      end.to_not raise_error
-      expect(ReactOnRails.configuration.i18n_dir).to eq(dir)
-    end
+    describe ".i18n_yml_dir" do
+      let(:i18n_yml_dir) { existing_path }
 
-    it "raises if the i18n yaml directory does not exist" do
-      junk_name = "/YYYY/junkYYYY"
-      expect do
-        ReactOnRails.configure do |config|
-          config.i18n_yml_dir = junk_name
-        end
-      end.to raise_error(/#{junk_name}/)
-    end
+      after do
+        ReactOnRails.configure { |config| config.i18n_yml_dir = nil }
+      end
 
-    it "does not raises if the i18n yaml directory does exist" do
-      dir = __dir__
-      expect do
-        ReactOnRails.configure do |config|
-          config.i18n_yml_dir = dir
-        end
-      end.to_not raise_error
-      expect(ReactOnRails.configuration.i18n_yml_dir).to eq(dir)
+      it "passes if directory exists" do
+        expect do
+          ReactOnRails.configure do |config|
+            config.i18n_yml_dir = i18n_yml_dir
+          end
+        end.not_to raise_error
+      end
+
+      it "fails with empty string value" do
+        expect do
+          ReactOnRails.configure do |config|
+            config.i18n_yml_dir = ""
+          end
+        end.to raise_error(RuntimeError, /invalid value for `config\.i18n_yml_dir`/)
+      end
+
+      it "fails with not existing directory" do
+        expect do
+          ReactOnRails.configure do |config|
+            config.i18n_yml_dir = not_existing_path
+          end
+        end.to raise_error(RuntimeError, /invalid value for `config\.i18n_yml_dir`/)
+      end
     end
 
     it "be able to config default configuration of the gem" do

spec/react_on_rails/locales_to_js_spec.rb
@@ -9,13 +9,6 @@ module ReactOnRails
     let(:translations_path) { "#{i18n_dir}/translations.js" }
     let(:default_path) { "#{i18n_dir}/default.js" }
 
-    it "with i18n_dir set to ''" do
-      ReactOnRails.configure do |config|
-        config.i18n_dir = ''
-      end
-      expect { ReactOnRails::LocalesToJs.new }.to raise_error(/config.i18n_dir is set and it is not a directory. Did you set config.i18n_dir in the react_on_rails initializer?/)
-    end
-    
     shared_examples "locale to js" do
       context "with obsolete js files" do
         before do