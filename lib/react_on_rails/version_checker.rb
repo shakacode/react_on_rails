f3a5828214f1b5e870c4ddacff5e7340ba2bd06d
Justin Gordon <justin.gordon@gmail.com>
2018-02-28 20:14:08 -1000

    Update version_checker.rb

--- lib/react_on_rails/version_checker.rb
+++ lib/react_on_rails/version_checker.rb
@@ -82,9 +82,7 @@ module ReactOnRails
       def major_minor_patch
         return if relative_path?
         match = raw.match(MAJOR_MINOR_PATCH_VERSION_REGEX)
-        unless match
-          raise "Cannot parse version number '#{raw}' (wildcard versions are not supported)"
-        end
+        raise "Cannot parse version number '#{raw}' (wildcard versions are not supported)" unless match
         [match[1], match[2], match[3]]
       end
 
