c7404059dc1b0c900d461daa7bedce78a3423c6f
Mario Perez <mapreal19@gmail.com>
2018-03-26 16:01:15 +0200

    Misc fix

--- spec/dummy_no_webpacker/spec/rails_helper.rb
+++ spec/dummy_no_webpacker/spec/rails_helper.rb
@@ -30,7 +30,7 @@ require "capybara-screenshot/rspec"
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   # Ensure that if we are running js tests, we are using latest webpack assets
