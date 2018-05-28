a23d3142b0b232dfc794e4c01c70ef1f182e22da
Hugo Chevalier <drakhaine@gmail.com>
2018-05-28 17:56:43 +0200

    Fix eslint offenses on HelloWorldRehydratable

--- spec/dummy/client/app/components/HelloWorldRehydratable.jsx
+++ spec/dummy/client/app/components/HelloWorldRehydratable.jsx
@@ -17,8 +17,8 @@ class HelloWorldRehydratable extends React.Component {
     super(props);
     this.state = props.helloWorldData;
     this.setNameDomRef = this.setNameDomRef.bind(this);
+    this.forceClientHydration = this.forceClientHydration.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    this.forceClientHydration = this.forceClientHydration.bind(this)
   }
 
   componentDidMount() {
@@ -29,6 +29,10 @@ class HelloWorldRehydratable extends React.Component {
     document.removeEventListener('hydrate', this.forceClientHydration);
   }
 
+  setNameDomRef(nameDomNode) {
+    this.nameDomRef = nameDomNode;
+  }
+
   forceClientHydration() {
     const registeredComponentName = 'HelloWorldRehydratable';
     const { railsContext } = this.props;
@@ -36,21 +40,17 @@ class HelloWorldRehydratable extends React.Component {
     // Target all instances of the component in the DOM
     const match = document.querySelectorAll(`[id^=${registeredComponentName}-react-component-]`);
     // Not all browsers support forEach on NodeList so we go with a classic for-loop
-    for (let i = 0; i < match.length; ++i) {
+    for (let i = 0; i < match.length; i += 1) {
       const component = match[i];
       // Get component specification <script> tag
-      const componentSpecificationTag = document.querySelector(`script[data-dom-id=${component.id}]`)
+      const componentSpecificationTag = document.querySelector(`script[data-dom-id=${component.id}]`);
       if (componentSpecificationTag) {
         // Read props from the component specification tag and merge railsContext
-        const mergedProps = {...JSON.parse(componentSpecificationTag.textContent), railsContext};
+        const mergedProps = { ...JSON.parse(componentSpecificationTag.textContent), railsContext };
         // Hydrate
         ReactOnRails.render(registeredComponentName, mergedProps, component.id);
       }
     }
-  }
-
-  setNameDomRef(nameDomNode) {
-    this.nameDomRef = nameDomNode;
   }
 
   handleChange() {
