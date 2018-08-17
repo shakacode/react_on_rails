1a0ea5d59ac9a44ebb073815f9fda14af68893e9
Garethp <blackfireweb@gmail.com>
2018-08-17 12:51:46 +0100

    Adding a forced generator option

--- node_package/src/ComponentRegistry.js
+++ node_package/src/ComponentRegistry.js
@@ -7,8 +7,9 @@ const registeredComponents = new Map();
 export default {
   /**
    * @param components { component1: component1, component2: component2, etc. }
+   * @param isGeneratorFunction boolean
    */
-  register(components) {
+  register(components, isGeneratorFunction) {
     Object.keys(components).forEach(name => {
       if (registeredComponents.has(name)) {
         console.warn('Called register for component that is already registered', name);
@@ -19,7 +20,7 @@ export default {
         throw new Error(`Called register with null component named ${name}`);
       }
 
-      const isGeneratorFunction = generatorFunction(component);
+      const isGeneratorFunction = isGeneratorFunction || generatorFunction(component);
       const isRenderer = isGeneratorFunction && component.length === 3;
 
       registeredComponents.set(name, {
