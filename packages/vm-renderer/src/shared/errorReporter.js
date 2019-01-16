0f418203c2a913424e2ac1523f0b6a32918f2b37
Justin Gordon <justin.gordon@gmail.com>
2019-01-15 23:14:44 -1000

    Add Honeybadger support

---
+++ packages/vm-renderer/src/shared/errorReporter.js
@@ -0,0 +1,34 @@
+import Honeybadger from 'honeybadger';
+
+class ErrorReporter {
+  constructor() {
+    this.honeybadger = false;
+  }
+
+  addHoneybadgerApiKey(apiKey) {
+    Honeybadger.configure({ apiKey });
+    this.honeybadger = true;
+  }
+
+  addSentryDsn(_sentryDsn) {
+    this.sentry = true;
+    throw new Error('Sentry is not yet supported.');
+  }
+
+  setContext(context) {
+    if (this.honeybadger) {
+      Honeybadger.setConext(context);
+    }
+  }
+
+  notify(msg, context = {}) {
+    console.log('ErrorReporter postMessage', msg);
+    if (this.honeybadger) {
+      Honeybadger.notify(msg, context);
+    }
+  }
+}
+
+const errorReporter = new ErrorReporter();
+
+export default errorReporter;
