be9c63dc5ebb6744b773060434c9905797acf944
Tahsin Hasan <tahsin352@yahoo.com>
2019-04-14 14:36:45 +0600

    travis ci reported issue regarding trailing whitespace fixed

--- lib/react_on_rails/helper.rb
+++ lib/react_on_rails/helper.rb
@@ -345,7 +345,6 @@ module ReactOnRails
       else
         content_tag_options_html_tag = "div"
       end
-      
       content_tag_options[:id] = render_options.dom_id
 
       rendered_output = content_tag(:"#{content_tag_options_html_tag}",
