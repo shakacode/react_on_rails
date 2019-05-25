e60734b9f93511f6d24daaca9c72152b927f854e
Andrey Ashgaliyev <a.ashgaliev@gmail.com>
2019-05-25 22:09:02 +0500

    Turn off prefer-default-export globally

--- packages/vm-renderer/src/ReactOnRailsProVmRenderer.js
+++ packages/vm-renderer/src/ReactOnRailsProVmRenderer.js
@@ -2,7 +2,6 @@ const cluster = require('cluster');
 const master = require('./master');
 const worker = require('./worker');
 
-/* eslint-disable import/prefer-default-export */
 exports.reactOnRailsProVmRenderer = function(config = {}) {
   if (cluster.isMaster) {
     master(config);
