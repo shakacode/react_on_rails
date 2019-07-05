ce7daeb889dd885b042b838a68568ac9b11b92a8
behrang <behraaang@gmail.com>
2019-07-05 20:27:45 +0430

    getting clientConfig look identical as before, 3/8 loaders, [url, file, expose]

--- spec/dummy/config/webpack/environment.js
+++ spec/dummy/config/webpack/environment.js
@@ -3,6 +3,7 @@ const sassResources = ['./client/app/assets/styles/app-variables.scss']
 const aliasConfig = require('./alias.js')
 const rules = environment.loaders
 const sassLoader = rules.get('sass')
+const urlFileSizeCutover = 1000; // below 10k, inline, small 1K is to test file loader
 
 sassLoader.use.push({
   loader: 'sass-resources-loader',
@@ -10,6 +11,30 @@ sassLoader.use.push({
     resources: sassResources
   },
 })
+
+const urlLoader = {
+    test: /\.(jpe?g|png|gif|ico|woff)$/,
+    use: {
+        loader: 'url-loader',
+        options: {
+            limit: urlFileSizeCutover,
+
+            // NO leading slash
+            name: 'images/[name]-[hash].[ext]',
+        },
+    },
+}
+// Insert json loader after/before a given loader
+environment.loaders.insert('url', urlLoader, { before: 'file'} )
+
+const exposeLoader = {
+    test: require.resolve('jquery'),
+    use: [ { loader: 'expose-loader', options: 'jQuery' } ]
+}
+// Insert json loader after/before a given loader
+environment.loaders.insert('expose', exposeLoader, { after: 'file'} )
+
+debugger
 
 environment.config.merge(aliasConfig)
 
