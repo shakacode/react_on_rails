d609868aa5306fabc92f67e48006798685bb296c
behrang <behraaang@gmail.com>
2019-07-13 00:05:34 +0430

    rules on webpacker configuration is exactly the same as custom config on master, respec: 93 examples, 4 failures

--- spec/dummy/config/webpack/environment.js
+++ spec/dummy/config/webpack/environment.js
@@ -7,6 +7,7 @@ const rules = environment.loaders
 const fileLoader = rules.get('file')
 const cssLoader = rules.get('css')
 const sassLoader = rules.get('sass')
+const babelLoader = rules.get('babel')
 const urlFileSizeCutover = 1000; // below 10k, inline, small 1K is to test file loader
 
 sassLoader.use.push({
@@ -22,6 +23,7 @@ console.log("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
 cssLoader.use[1].options.modules = true
 sassLoader.use[1].options.modules = true
 fileLoader.test = /\.(ttf|eot|svg)$/
+fileLoader.use[0].options = { name: 'images/[name]-[hash].[ext]' }
 
 const urlLoader = {
     test: /\.(jpe?g|png|gif|ico|woff)$/,
@@ -35,15 +37,27 @@ const urlLoader = {
         },
     },
 }
-// Insert json loader after/before a given loader
 environment.loaders.insert('url', urlLoader, { before: 'file'} )
 
 const exposeLoader = {
     test: require.resolve('jquery'),
     use: [ { loader: 'expose-loader', options: 'jQuery' } ]
 }
-// Insert json loader after/before a given loader
 environment.loaders.insert('expose', exposeLoader, { after: 'file'} )
+
+const reactLoader = {
+    test: require.resolve('react'),
+    use: [ { loader: 'imports-loader', options: { shim: 'es5-shim/es5-shim', sham: 'es5-shim/es5-sham' } } ]
+}
+environment.loaders.insert('react', reactLoader, { after: 'sass'} )
+
+const jqueryujsLoader = {
+    test: require.resolve('jquery-ujs'),
+    use: [ { loader: 'imports-loader', options: { jQuery: 'jquery' } } ]
+}
+environment.loaders.insert('jquery-ujs', jqueryujsLoader, { after: 'react'} )
+
+environment.loaders.insert('babel', babelLoader, { before: 'css'} )
 
 environment.config.merge(aliasConfig)
 
