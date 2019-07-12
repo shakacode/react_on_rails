8cf55066269452d228572a9c551d05e5e121ed23
behrang <behraaang@gmail.com>
2019-07-13 00:59:49 +0430

    DefinePlugin added, ManifestPlugin manipulation, dev, test and production config include configs

--- spec/dummy/config/webpack/development.js
+++ spec/dummy/config/webpack/development.js
@@ -4,10 +4,19 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development"
 // (for SSR of React components). This is easy enough as we can export arrays of webpack configs.
 const environment = require("./environment")
 const serverConfig = require("./server")
+const merge = require("webpack-merge")
 
 environment.splitChunks((config) => Object.assign({}, config, { optimization: { splitChunks: false }}))
 
-module.exports = [environment.toWebpackConfig(), serverConfig]
+const clientEnvironment = merge(environment.toWebpackConfig(), {
+    entry: {
+        'vendor-bundle': [
+            'jquery-ujs',
+        ],
+    },
+})
+
+module.exports = [clientEnvironment, serverConfig]
 
 // If you just want to test the client config without building the server config
 // module.exports = environment.toWebpackConfig()
