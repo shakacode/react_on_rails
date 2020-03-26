f393430ba0e13ff7b618985c480778af26e08615
Justin Gordon <justin.gordon@gmail.com>
2020-03-25 15:13:24 -1000

    Better handling for an invalid renderer_url configuration (#109)

--- lib/react_on_rails_pro/request.rb
+++ lib/react_on_rails_pro/request.rb
@@ -124,6 +124,18 @@ module ReactOnRailsPro
           force_retry: true,
           url: ReactOnRailsPro.configuration.renderer_url
         )
+      rescue StandardError => e
+        message = <<~MSG
+          [ReactOnRailsPro] Error creating PersistentHTTP connection.
+          renderer_http_pool_size = #{ReactOnRailsPro.configuration.renderer_http_pool_size}
+          renderer_http_pool_timeout = #{ReactOnRailsPro.configuration.renderer_http_pool_timeout}
+          renderer_http_pool_warn_timeout = #{ReactOnRailsPro.configuration.renderer_http_pool_warn_timeout}
+          renderer_url = #{ReactOnRailsPro.configuration.renderer_url}
+          Be sure to use a url that contains the protocol of http or https.
+          Original error is
+          #{e}
+        MSG
+        raise ReactOnRailsPro::Error, message
       end
     end
   end
