fbfdd0b8219b278024916d3ae59de36137257514
hussainakram <hussain.akram128@gmail.com>
2020-08-07 18:15:19 +0500

    update webpack configs

--- spec/dummy/config/webpack/development.js
+++ spec/dummy/config/webpack/development.js
@@ -1,34 +1,9 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-// We need to compile both our development JS (for serving to the client) and our server JS
-// (for SSR of React components). This is easy enough as we can export arrays of webpack configs.
-const clientEnvironment = require('./client');
-const serverConfig = require('./server');
-const merge = require('webpack-merge');
+const webpackConfig = require('./webpackConfig');
 
-const optimization = {
-  splitChunks: {
-    chunks: 'async',
-    cacheGroups: {
-      vendor: {
-        chunks: 'async',
-        name: 'vendor',
-        test: 'vendor',
-        enforce: true,
-      },
-    },
-  },
+const developmentOnly = () => {
+  // place any code here that is for development only
 };
 
-clientEnvironment.splitChunks((config) => Object.assign({}, config, { optimization: optimization }));
-
-const clientConfig = clientEnvironment.toWebpackConfig();
-
-// For HMR, we need to separate the the client and server webpack configurations
-if (process.env.WEBPACK_DEV_SERVER) {
-  module.exports = clientConfig;
-} else if (process.env.SERVER_BUNDLE_ONLY) {
-  module.exports = serverConfig;
-} else {
-  module.exports = [clientConfig, serverConfig];
-}
+module.exports = webpackConfig(developmentOnly);
--- spec/dummy/config/webpack/server.js
+++ spec/dummy/config/webpack/server.js
@@ -1,38 +1,85 @@
+const { config } = require('@rails/webpacker');
 const environment = require('./environment');
 const merge = require('webpack-merge');
+
 const devBuild = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 const webpack = require('webpack');
 
+const clientConfigObject = environment.toWebpackConfig();
 // React Server Side Rendering webpacker config
 // Builds a Node compatible file that React on Rails can load, never served to the client.
+const configureServer = () => {
+  // We need to use "merge" because the clientConfigObject, EVEN after running
+  // toWebpackConfig() is a mutable GLOBAL. Thus any changes, like modifying the
+  // entry value will result in changing the client config!
+  // Using webpack-merge into an empty object avoids this issue.
+  const serverWebpackConfig = merge({}, clientConfigObject);
 
-environment.plugins.insert(
-  'DefinePlugin',
-  new webpack.DefinePlugin({
-    TRACE_TURBOLINKS: true,
-    'process.env': {
-      NODE_ENV: devBuild,
-    },
-  }),
-  { after: 'Environment' },
-);
-const serverConfig = merge(environment.toWebpackConfig(), {
-  target: 'web',
-  entry: './client/app/startup/serverRegistration.jsx',
-  output: {
+  // We just want the single server bundle entry
+  // const serverEntry = {
+  //   'server-bundle': environment.entry.get('server-bundle'),
+  // }
+  serverWebpackConfig.entry = './client/app/startup/serverRegistration.jsx';
+
+  // Remove the mini-css-extract-plugin from the style loaders because
+  // the client build will handle exporting CSS.
+  // replace file-loader with null-loader
+  serverWebpackConfig.module.rules.forEach(loader => {
+    if (loader.use && loader.use.filter) {
+      loader.use = loader.use.filter(
+        item => !(typeof item === 'string' && item.match(/mini-css-extract-plugin/)),
+      );
+    }
+  });
+
+  // Custom output for the server-bundle that matches the config in
+  // config/initializers/react_on_rails.rb
+  serverWebpackConfig.output = {
     filename: 'server-bundle.js',
-
-    // https://webpack.js.org/configuration/output/#outputglobalobject
     globalObject: 'this',
-  },
-  optimization: {
+    // if using a node server renderer, uncomment the next line
+    libraryTarget: 'commonjs2',
+    path: config.outputPath,
+    publicPath: config.outputPath,
+    // https://webpack.js.org/configuration/output/#outputglobalobject
+  };
+
+  // No splitting of chunks for a server bundle
+  serverWebpackConfig.optimization = {
     minimize: false,
-  },
-});
+  };
+  serverWebpackConfig.plugins.unshift(new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }));
 
-// Don't hash the server bundle. No need.
-serverConfig.plugins = serverConfig.plugins.filter(
-  (plugin) => plugin.constructor.name !== 'WebpackAssetsManifest',
-);
+  serverWebpackConfig.plugins.push(
+    new webpack.DefinePlugin({
+      TRACE_TURBOLINKS: true,
+      'process.env': {
+        NODE_ENV: devBuild,
+      },
+    }),
+  );
 
-module.exports = serverConfig;
+  // Don't hash the server bundle b/c would conflict with the client manifest
+  // And no need for the MiniCssExtractPlugin
+  serverWebpackConfig.plugins = serverWebpackConfig.plugins.filter(
+    plugin =>
+      plugin.constructor.name !== 'WebpackAssetsManifest' &&
+      plugin.constructor.name !== 'MiniCssExtractPlugin',
+  );
+
+  // Critical due to https://github.com/rails/webpacker/pull/2644
+  delete serverWebpackConfig.devServer;
+
+  // eval works well for the SSR bundle because it's the fastest and shows
+  // lines in the server bundle which is good for debugging SSR
+  // The default of cheap-module-source-map is slow and provides poor info.
+  serverWebpackConfig.devtool = 'eval';
+
+  // If we use 'web', then libraries like Emotion and loadable-components break with SSR
+  // if using a node server renderer, uncomment the next line
+  // serverWebpackConfig.target = 'node'
+
+  return serverWebpackConfig;
+};
+
+module.exports = configureServer;
