4a827b008ce66699eb5d843d43a62faebe182e8d
Justin Gordon <justin.gordon@gmail.com>
2020-09-29 22:45:14 -1000

    Remove migration check

--- spec/dummy/spec/rails_helper.rb
+++ spec/dummy/spec/rails_helper.rb
@@ -34,7 +34,7 @@ require "capybara-screenshot/rspec"
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!
+# ActiveRecord::Migration.maintain_test_schema!
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
