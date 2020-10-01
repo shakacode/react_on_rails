95b4e0551da903302b88173c821bb6f99216fb82
Justin Gordon <justin.gordon@gmail.com>
2020-09-30 20:52:09 -1000

    Just doc updates.

--- lib/react_on_rails/helper.rb
+++ lib/react_on_rails/helper.rb
@@ -33,7 +33,7 @@ module ReactOnRails
     #     a generator:
     #   See README.md for how to "register" your react components.
     #   See spec/dummy/client/app/packs/server-bundle.js and
-    #     spec/dummy/client/app/startup/client-bundle.js for examples of this
+    #     spec/dummy/client/app/packs/client-bundle.js for examples of this.
     #
     # options:
     #   props: Ruby Hash or JSON string which contains the properties to pass to the react object. Do
