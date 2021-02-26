90b57ccac97f69c393696b555e29436b2230ccca
Judah Meek <judah.meek@gmail.com>
2021-02-26 13:04:38 -0600

    Refactor hash digest logic into digest_of_globs method (#166)
    Allows reuse of this method.

--- react_on_rails_pro/lib/react_on_rails_pro/utils.rb
+++ react_on_rails_pro/lib/react_on_rails_pro/utils.rb
@@ -18,6 +18,18 @@ module ReactOnRailsPro
       ReactOnRailsPro::Request.upload_assets
     end
 
+    # takes an array of globs & returns a md5 hash
+    def self.digest_of_globs(globs)
+      # NOTE: Dir.glob is not stable between machines, even with same OS. So we must sort.
+      # .uniq was added to remove redundancies in the case digest_of_globs is used on a union of
+      # dependency_globs & source code in order to create a cache key for production bundles
+      # We've tested it to make sure that it adds less than a second even in the case of thousands of files
+      files = Dir.glob(globs).uniq.sort!
+      digest = Digest::MD5.new
+      files.each { |f| digest.file(f) }
+      digest.hexdigest
+    end
+
     # Returns a string which should be used as a component in any cache key for
     # react_component or react_component_hash when server rendering. This value is either
     # the server bundle filename with the hash from webpack or an MD5 digest of the
