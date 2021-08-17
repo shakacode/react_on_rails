e7c2544b5884f0e1c250f0d2784bf86c6a8edc0d
Judah Meek <judah.meek@gmail.com>
2021-08-17 06:25:21 -0500

    Sync Integration Specs with React on Rails Pro (#1382)

--- spec/dummy/client/app/components/RouterFirstPage.jsx
+++ spec/dummy/client/app/components/RouterFirstPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const RouterFirstPage = () => (
   <div className="container">
-    <h2>React Router First Page</h2>
+    <h2 id="first-page">React Router First Page</h2>
     <p>This page brought to you by React Router</p>
   </div>
 );
