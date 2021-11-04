e06a5a8cca405c4ee971aaa9d99e2cf0a79c7e7f
Gus <gsc13@tuta.io>
2021-11-04 17:29:12 -0300

    Add config.build_production_command to dummy

--- spec/dummy/config/initializers/react_on_rails.rb
+++ spec/dummy/config/initializers/react_on_rails.rb
@@ -16,6 +16,8 @@ module RenderingExtension
 end
 
 ReactOnRails.configure do |config|
+  config.build_production_command = "yarn build:rescript"
+
   config.server_bundle_js_file = "server-bundle.js"
   config.random_dom_id = false # default is true
 
