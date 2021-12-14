6e44563c1652c0cba94a5257ebe9bd1c83f19920
Gus <gsc13@tuta.io>
2021-12-14 11:56:04 -0300

    Run prettier, Update yarn.lock

--- spec/dummy/babel.config.js
+++ spec/dummy/babel.config.js
@@ -1,14 +1,12 @@
 module.exports = function (api) {
-  const defaultConfigFunc = require('@rails/webpacker/package/babel/preset.js')
+  const defaultConfigFunc = require('@rails/webpacker/package/babel/preset.js');
   const resultConfig = defaultConfigFunc(api);
 
   const changesOnDefault = {
-    plugins: [
-      process.env.WEBPACK_SERVE && 'react-refresh/babel'
-    ].filter(Boolean),
-  }
+    plugins: [process.env.WEBPACK_SERVE && 'react-refresh/babel'].filter(Boolean),
+  };
 
-  resultConfig.plugins = [...resultConfig.plugins, ...changesOnDefault.plugins ];
+  resultConfig.plugins = [...resultConfig.plugins, ...changesOnDefault.plugins];
 
   return resultConfig;
-}
+};
