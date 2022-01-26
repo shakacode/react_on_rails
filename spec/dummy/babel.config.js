124c7a475f0429aa0aaa86f8a031bec26172f379
Gus <gsc13@tuta.io>
2022-01-26 15:28:04 -0300

    Run prettier

--- spec/dummy/babel.config.js
+++ spec/dummy/babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
-  const defaultConfigFunc = require('shakapacker/package/babel/preset.js')
-  const resultConfig = defaultConfigFunc(api)
-  const isProductionEnv = api.env('production')
+  const defaultConfigFunc = require('shakapacker/package/babel/preset.js');
+  const resultConfig = defaultConfigFunc(api);
+  const isProductionEnv = api.env('production');
 
   const changesOnDefault = {
     presets: [
@@ -9,22 +9,23 @@ module.exports = function (api) {
         '@babel/preset-react',
         {
           development: !isProductionEnv,
-          useBuiltIns: true
-        }
-      ]
+          useBuiltIns: true,
+        },
+      ],
     ].filter(Boolean),
     plugins: [
       process.env.WEBPACK_SERVE && 'react-refresh/babel',
-      isProductionEnv && ['babel-plugin-transform-react-remove-prop-types',
+      isProductionEnv && [
+        'babel-plugin-transform-react-remove-prop-types',
         {
-          removeImport: true
-        }
-      ]
+          removeImport: true,
+        },
+      ],
     ].filter(Boolean),
-  }
+  };
 
-  resultConfig.presets = [...resultConfig.presets, ...changesOnDefault.presets]
-  resultConfig.plugins = [...resultConfig.plugins, ...changesOnDefault.plugins ]
+  resultConfig.presets = [...resultConfig.presets, ...changesOnDefault.presets];
+  resultConfig.plugins = [...resultConfig.plugins, ...changesOnDefault.plugins];
 
-  return resultConfig
-}
+  return resultConfig;
+};
