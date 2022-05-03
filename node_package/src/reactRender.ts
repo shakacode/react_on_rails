1825db395524786e53010cec0abacfc7fea40c9e
Ari Summer <aribsummer@gmail.com>
2022-05-02 21:04:57 -0600

    Store modules in variables to force webpack dynamic require

--- node_package/src/reactRender.ts
+++ node_package/src/reactRender.ts
@@ -1,8 +1,10 @@
 import { ReactElement, Component } from 'react';
 import supportsReactCreateRoot from './supportsReactCreateRoot';
 
-// eslint-disable-next-line import/no-unresolved
-const ReactDOM = supportsReactCreateRoot ? require("react-dom/client") : require("react-dom");
+const reactDomClient = "react-dom/client";
+const reactDom = "react-dom";
+// eslint-disable-next-line import/no-dynamic-require
+const ReactDOM = require(supportsReactCreateRoot ? reactDomClient : reactDom);
 
 export default function reactRender(domNode: Element, reactElement: ReactElement): void | Element | Component {
   if (supportsReactCreateRoot) {
