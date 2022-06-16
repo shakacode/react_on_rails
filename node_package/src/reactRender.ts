e68ad9b86c944dd4fcf1edf750f2db82544b14e2
Ari Summer <aribsummer@gmail.com>
2022-06-16 09:31:28 -0600

    Switch require to use string interpolation

--- node_package/src/reactRender.ts
+++ node_package/src/reactRender.ts
@@ -1,10 +1,8 @@
 import { ReactElement, Component } from 'react';
 import supportsReactCreateRoot from './supportsReactCreateRoot';
 
-const reactDomClient = "react-dom/client";
-const reactDom = "react-dom";
 // eslint-disable-next-line import/no-dynamic-require
-const ReactDOM = require(supportsReactCreateRoot ? reactDomClient : reactDom);
+const ReactDOM = require(`react-dom${supportsReactCreateRoot ? '/client' : ''}`);
 
 export default function reactRender(domNode: Element, reactElement: ReactElement): void | Element | Component {
   if (supportsReactCreateRoot) {
