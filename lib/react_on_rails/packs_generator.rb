40a21f977b21aa1a68e0a33ff2b195f097e6b56b
pulkitkkr <pulkitkkr@gmail.com>
2022-07-07 19:54:22 -0400

    Refactor Code

--- lib/react_on_rails/packs_generator.rb
+++ lib/react_on_rails/packs_generator.rb
@@ -14,42 +14,8 @@ module ReactOnRails
     end
 
     def self.generate_packs
-      is_server_rendering_enabled = ReactOnRails.configuration.server_bundle_js_file.present?
-
-      return common_components.each_value { |p| create_pack(p) } unless is_server_rendering_enabled
-
-      client_components.each_value { |p| create_pack(p) }
-      create_server_pack
-    end
-
-    def self.create_server_pack
-      generated_server_bundle_file_name = component_name(defined_server_bundle_file.sub(".js", "-genrated.js"))
-      generated_server_bundle_file = "#{generated_packs_directory}/#{generated_server_bundle_file_name}.js"
-
-      server_component_imports = server_components.map do |_p, v|
-        "import #{component_name(v)} from '#{relative_component_path(v, generated_server_bundle_file)}';"
-      end
-
-      components_to_register = server_components.map { |_p, v| component_name(v) }
-      already_present_server_bundle_relative_path = relative_component_path(defined_server_bundle_file,
-                                                                            generated_server_bundle_file)
-
-      content = <<~FILE_CONTENT
-        /* eslint-disable */
-        import ReactOnRails from 'react-on-rails';
-        import "#{already_present_server_bundle_relative_path}"
-
-        #{server_component_imports.join("\n")}
-
-        ReactOnRails.register({#{components_to_register.join(",\n")}});
-        /* eslint-enable */
-      FILE_CONTENT
-
-      f = File.new(generated_server_bundle_file, "w")
-      f.puts(content)
-      f.close
-
-      puts(Rainbow("Generated Server Bundle: #{generated_server_bundle_file}").orange)
+      client_component_to_path.each_value { |component_path| create_pack(component_path) }
+      create_server_pack if ReactOnRails.configuration.server_bundle_js_file.present?
     end
 
     def self.create_pack(file_path)
@@ -68,43 +34,94 @@ module ReactOnRails
       <<~FILE_CONTENT
         /* eslint-disable */
         import ReactOnRails from 'react-on-rails';
-        import #{registered_component_name} from '#{relative_component_path(file_path)}';
+        import #{registered_component_name} from '#{relative_component_path_from_generated_pack(file_path)}';
 
         ReactOnRails.register({#{registered_component_name}});
         /* eslint-enable */
       FILE_CONTENT
     end
 
-    def self.clean_generated_packs_directory
-      FileUtils.rm_rf(generated_packs_directory)
-      FileUtils.mkdir_p generated_packs_directory
+    def self.create_server_pack
+      f = File.new(generated_server_bundle_file_path, "w")
+      f.puts(generated_server_pack_file_content)
+      f.close
+
+      add_generated_pack_to_server_bundle
+      puts(Rainbow("Generated Server Bundle: #{generated_server_bundle_file_path}").orange)
     end
 
-    def defined_server_bundle_file
+    def self.generated_server_pack_file_content
+      server_component_imports = server_component_to_path.map do |name, component_path|
+        "import #{name} from '#{relative_path(generated_server_bundle_file_path, component_path)}';"
+      end
+
+      components_to_register = server_component_to_path.keys
+
+      <<~FILE_CONTENT
+        /* eslint-disable */
+        import ReactOnRails from 'react-on-rails';
+
+        #{server_component_imports.join("\n")}
+
+        ReactOnRails.register({#{components_to_register.join(",\n")}});
+        /* eslint-enable */
+      FILE_CONTENT
+    end
+
+    def self.add_generated_pack_to_server_bundle
+      relative_path_to_generated_server_bundle = relative_path(defined_server_bundle_file_path,
+                                                               generated_server_bundle_file_path)
+      content = <<~FILE_CONTENT
+        // eslint-disable-next-line import/extensions
+        import  "./#{relative_path_to_generated_server_bundle}"\n
+      FILE_CONTENT
+
+      prepend_to_file_if_not_present(defined_server_bundle_file_path, content)
+    end
+
+    def self.generated_server_bundle_file_path
+      generated_server_bundle_file_name = component_name(defined_server_bundle_file_path.sub(".js", "-genrated.js"))
+
+      "#{source_entry_path}/#{generated_server_bundle_file_name}.js"
+    end
+
+    def self.clean_generated_packs_directory
+      FileUtils.rm_rf(generated_packs_directory_path)
+      FileUtils.mkdir_p generated_packs_directory_path
+    end
+
+    def self.defined_server_bundle_file_path
       server_bundle_file_name = ReactOnRails.configuration.server_bundle_js_file
 
       Dir.glob("#{source_entry_path}/**/#{server_bundle_file_name}").first
     end
 
-    def self.generated_packs_directory
+    def self.generated_packs_directory_path
       "#{source_entry_path}/generated"
     end
 
-    def source_entry_path
+    def self.source_entry_path
       ReactOnRails::WebpackerUtils.webpacker_source_entry_path
     end
 
-    def self.relative_component_path(file_path, source_path = nil)
-      generated_file_path = Pathname.new generated_pack_path(source_path.present? ? source_path : file_path)
-      component_file_path = Pathname.new file_path
+    def self.relative_component_path_from_generated_pack(ror_component_path)
+      component_file_path = Pathname.new ror_component_path
+      generated_pack_path = Pathname.new generated_pack_path(ror_component_path)
+
+      relative_path(generated_pack_path, component_file_path)
+    end
+
+    def self.relative_path(from, to)
+      from_path = Pathname.new from
+      to_path = Pathname.new to
 
       # TODO: Debug Relative Path always has extra '../'
-      relative_path = component_file_path.relative_path_from generated_file_path
+      relative_path = to_path.relative_path_from from_path
       relative_path.sub("../", "")
     end
 
     def self.generated_pack_path(file_path)
-      "#{generated_packs_directory}/#{component_name(file_path)}.jsx"
+      "#{generated_packs_directory_path}/#{component_name(file_path)}.jsx"
     end
 
     def self.component_name(file_path)
@@ -113,26 +130,26 @@ module ReactOnRails
       basename.sub(ENDS_WITH_CLIENT_OR_SERVER_REGEX, "")
     end
 
-    def self.component_name_path_hash(paths)
+    def self.component_name_to_path(paths)
       paths.to_h { |path| [component_name(path), path] }
     end
 
-    def self.common_components
-      common_components_paths = Dir.glob("#{components_search_path}/*")
-      component_name_path_hash(common_components_paths)
+    def self.common_component_to_path
+      common_components_paths = Dir.glob("#{components_search_path}/*[!]*")
+      component_name_to_path(common_components_paths)
     end
 
-    def self.client_components
+    def self.client_component_to_path
       client_render_components_paths = Dir.glob("#{components_search_path}/*.client.*")
-      client_specific_components = component_name_path_hash(client_render_components_paths)
-      common_components.merge(client_specific_components)
+      client_specific_components = component_name_to_path(client_render_components_paths)
+      common_component_to_path.merge(client_specific_components)
     end
 
-    def self.server_components
+    def self.server_component_to_path
       server_render_components_paths = Dir.glob("#{components_search_path}/*.server.*")
-      server_specific_components = component_name_path_hash(server_render_components_paths)
+      server_specific_components = component_name_to_path(server_render_components_paths)
 
-      common_components.merge(server_specific_components)
+      common_component_to_path.merge(server_specific_components)
     end
 
     def self.components_search_path
@@ -140,6 +157,20 @@ module ReactOnRails
       source_path = ReactOnRails::WebpackerUtils.webpacker_source_path
 
       "#{source_path}/**/#{components_directory}"
+    end
+
+    def self.prepend_to_file_if_not_present(file, str)
+      content = ""
+      File.open(file, "r") do |fd|
+        contents = fd.read
+        content += contents
+      end
+
+      return if content.start_with? str
+
+      File.open(file, "w") do |fd|
+        fd.write(str + content)
+      end
     end
   end
 end
