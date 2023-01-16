513ed0499f67b69e91a7f94d93b9ec032948737b
Mikhail Boutylin <lahmataja.pa4vara@gmail.com>
2023-01-16 22:44:09 +0400

    ControlPlane deployment (#311)
    * #294 added support for deployment to ControlPlane

--- spec/dummy/config/environments/production.rb
+++ spec/dummy/config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
