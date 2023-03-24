1b3385f3000adec71fa63ceb932be46d01943792
Judah Meek <judah.meek@gmail.com>
2023-03-24 04:18:10 -0500

    upgrade webdrivers

--- spec/dummy/spec/support/selenium_logger.rb
+++ spec/dummy/spec/support/selenium_logger.rb
@@ -4,32 +4,25 @@ RSpec.configure do |config|
   config.after(:each, :js) do |example|
     next unless %i[selenium_chrome selenium_chrome_headless].include?(Capybara.current_driver)
 
-    # As of 2018-10-21, traping errors does not work for firefox
-
-    log_only_list = %w[DEBUG INFO]
-    log_only_list += %w[WARNING SEVERE ERROR] if example.metadata[:ignore_js_errors]
-
     errors = []
 
-    page.driver.browser.manage.logs.get(:browser).each do |entry|
-      # can't use match? on Ruby 2.2. Uncomment when Ruby 2.4 is used for all test platforms
-      # next if entry.message.match?(/Download the React DevTools for a better development experience/)
-      next if entry.message.include?("Download the React DevTools for a better development experience")
+    non_error_levels = %w[DEBUG INFO WARNING]
 
-      log_only_list.include?(entry.level) ? puts(entry.message) : errors << entry.message
+    page.driver.browser.logs.get(:browser).each do |entry|
+
+      pretty_message = if entry.message.match?(%r{http://(127.0.0.1|app.lvh.me)[^ ]+ [\d:]+ })
+                         entry.message[/[^ ]+ [^ ]+ (.*)$/, 1]&.gsub(/\A"|"\Z/, "")&.gsub(/\\n/, "\n")
+                       else
+                         entry.message
+                       end
+
+      non_error_levels.include?(entry.level) ? puts(pretty_message) : errors << pretty_message
     end
 
-    page.driver.browser.manage.logs.get(:driver).each do |entry|
-      log_only_list.include?(entry.level) ? puts(entry.message) : errors << entry.message
+    page.driver.browser.logs.get(:driver).each do |entry|
+      non_error_levels.include?(entry.level) ? puts(entry.message) : errors << entry.message
     end
 
-    # https://stackoverflow.com/questions/60114639/timed-out-receiving-message-from-renderer-0-100-log-messages-using-chromedriver
-    clean_errors = errors.reject do |err_msg|
-      err_msg.include?("Timed out receiving message from renderer: 0.100") ||
-        err_msg.include?("SharedArrayBuffer will require cross-origin isolation") ||
-        err_msg.include?("You are currently using minified code outside of NODE_ENV === \\\"production\\\"")
-    end
-
-    raise("Java Script Error(s) on the page:\n\n#{errors.join("\n")}") if clean_errors.present?
+    raise("Java Script Error(s) on the page:\n\n#{errors.join('\n')}") if errors.present?
   end
 end
