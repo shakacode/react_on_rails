efa47aea39712482c53e6c68d792fef1eac2d97c
Judah Meek <judah.meek@gmail.com>
2023-03-24 04:21:11 -0500

    add missing generated entrypoints

--- spec/dummy/client/app/startup/ImageExample.jsx
+++ spec/dummy/client/app/startup/ImageExample.jsx
@@ -1,39 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import css from '../components/ImageExample/ImageExample.module.scss';
 
-// Note the global alias for image
-// eslint-disable-next-line import/no-unresolved
+// Note the global alias for images
 import logo from 'Assets/images/256egghead.png';
-// eslint-disable-next-line import/no-unresolved
+import bowerLogo from '../components/ImageExample/bower.png';
+import blueprintIcon from '../components/ImageExample/blueprint_icon.svg';
 import legoIcon from 'Assets/images/lego_icon.svg';
-
-import css from './ImageExample.module.scss';
-import bowerLogo from './bower.png';
-import blueprintIcon from './blueprint_icon.svg';
-
-const TestComponent = (_props) => (
+const TestComponent = (props) => (
   <div>
     <h1 className={css.red}>This is a test of CSS module color red.</h1>
     <hr />
     <h1 className={css.background}>Here is a label with a background-image from the CSS modules imported</h1>
-    <img src={logo} alt="logo" />
+    <img src={logo} />
     <hr />
     <h1 className={css.backgroundSameDirectory}>
       This label has a background image from the same directory. Below is an img tag in the same directory
     </h1>
-    <img src={bowerLogo} alt="bower logo" />
+    <img src={bowerLogo} />
     <hr />
     <h1> Below is an img tag of a svg in the same directory</h1>
-    <img src={blueprintIcon} alt="blueprint icon" />
+    <img src={blueprintIcon} />
     <hr />
     <h1>Below is a div with a background svg</h1>
-    <div className={css.googleLogo} alt="google logo" />
+    <div className={css.googleLogo} />
     <hr />
     <h1>SVG lego icon img tag with global path</h1>
-    <img src={legoIcon} alt="lego icon" />
+    <img src={legoIcon} />
     <hr />
     <h1>SVG lego icon with background image to global path</h1>
-    <div className={css.legoIcon} alt="lego icon again" />
+    <div className={css.legoIcon} />
   </div>
 );
+
+TestComponent.propTypes = {
+  message: PropTypes.string,
+};
 
 export default TestComponent;
