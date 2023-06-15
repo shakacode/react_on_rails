ba3ee783907351bf38cce1393a44ae9a278be43d
Judah Meek <judah.meek@gmail.com>
2023-06-15 02:05:40 -0500

    linting

--- lib/react_on_rails/packer_utils.rb
+++ lib/react_on_rails/packer_utils.rb
@@ -47,6 +47,7 @@ module ReactOnRails
 
     def self.shakapacker_version
       return @shakapacker_version if defined?(@shakapacker_version)
+
       @shakapacker_version = nil unless ReactOnRails::Utils.gem_available?("shakapacker")
       @shakapacker_version = Gem.loaded_specs["shakapacker"].version.to_s
     end
