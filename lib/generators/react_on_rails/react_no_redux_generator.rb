3e89097c8556aa3a2f6f985ecdad7176d9ba31b8
Mostafa Ahangarha <ahangarha@riseup.net>
2023-06-27 15:39:25 +0330

    Update install generator to support SP7 as well

--- lib/generators/react_on_rails/react_no_redux_generator.rb
+++ lib/generators/react_on_rails/react_no_redux_generator.rb
@@ -11,13 +11,11 @@ module ReactOnRails
       source_root(File.expand_path("templates", __dir__))
 
       def copy_base_files
-        base_js_path = "base/base"
         base_files = %w[app/javascript/bundles/HelloWorld/components/HelloWorld.jsx]
-        base_files.each { |file| copy_file("#{base_js_path}/#{file}", file) }
+        base_files.each { |file| copy_file("#{base_path}/#{file}", file) }
       end
 
       def create_appropriate_templates
-        base_path = "base/base"
         config = {
           component_name: "HelloWorld",
           app_relative_path: "../bundles/HelloWorld/components/HelloWorld"
@@ -27,6 +25,20 @@ module ReactOnRails
                  "app/javascript/packs/hello-world-bundle.js", config)
         template("#{base_path}/app/views/hello_world/index.html.erb.tt",
                  "app/views/hello_world/index.html.erb", config)
+      end
+
+      private
+
+      def base_path
+        @base_path ||= using_shakapacker_7? ? "base/shakapacker7_base/" : "base/webpacker_base/"
+      end
+
+      def using_shakapacker_7?
+        shakapacker_gem = Gem::Specification.find_by_name("shakapacker")
+        shakapacker_gem.version.segments.first == 7
+      rescue Gem::MissingSpecError
+        # In case using Webpacker
+        false
       end
     end
   end
