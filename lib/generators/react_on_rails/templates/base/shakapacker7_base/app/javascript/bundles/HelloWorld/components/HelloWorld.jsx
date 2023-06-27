997af9ac7de53b2a8b1040110960d7ac4ba45a2a
Mostafa Ahangarha <ahangarha@riseup.net>
2023-06-27 15:38:53 +0330

    Add SP7 base template

---
+++ lib/generators/react_on_rails/templates/base/shakapacker7_base/app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import style from './HelloWorld.module.css';
+
+const HelloWorld = (props) => {
+  const [name, setName] = useState(props.name);
+
+  return (
+    <div>
+      <h3>Hello, {name}!</h3>
+      <hr />
+      <form>
+        <label className={style.bright} htmlFor="name">
+          Say hello to:
+          <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+      </form>
+    </div>
+  );
+};
+
+HelloWorld.propTypes = {
+  name: PropTypes.string.isRequired, // this is passed from the Rails view
+};
+
+export default HelloWorld;
