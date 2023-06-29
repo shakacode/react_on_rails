e6a1305f369650005dac012beb52deb8a0df655c
Mostafa Ahangarha <ahangarha@riseup.net>
2023-06-29 12:09:41 +0330

    Undo extraction of base_path variable

--- lib/generators/react_on_rails/react_no_redux_generator.rb
+++ lib/generators/react_on_rails/react_no_redux_generator.rb
@@ -10,22 +10,22 @@ module ReactOnRails
       Rails::Generators.hide_namespace(namespace)
       source_root(File.expand_path("templates", __dir__))
 
-      BASE_PATH = "base/base"
-
       def copy_base_files
+        base_js_path = "base/base"
         base_files = %w[app/javascript/bundles/HelloWorld/components/HelloWorld.jsx]
-        base_files.each { |file| copy_file("#{BASE_PATH}/#{file}", file) }
+        base_files.each { |file| copy_file("#{base_js_path}/#{file}", file) }
       end
 
       def create_appropriate_templates
+        base_path = "base/base"
         config = {
           component_name: "HelloWorld",
           app_relative_path: "../bundles/HelloWorld/components/HelloWorld"
         }
 
-        template("#{BASE_PATH}/app/javascript/packs/registration.js.tt",
+        template("#{base_path}/app/javascript/packs/registration.js.tt",
                  "app/javascript/packs/hello-world-bundle.js", config)
-        template("#{BASE_PATH}/app/views/hello_world/index.html.erb.tt",
+        template("#{base_path}/app/views/hello_world/index.html.erb.tt",
                  "app/views/hello_world/index.html.erb", config)
       end
     end
