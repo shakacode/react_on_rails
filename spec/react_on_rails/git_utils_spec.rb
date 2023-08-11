efbb038588c93df53da0377fe63b645efcd21d3b
Judah Meek <judah.meek@gmail.com>
2023-08-11 14:58:18 -0500

    disable RSpec/VerifiedDoubleReference for MessageHandler

--- spec/react_on_rails/git_utils_spec.rb
+++ spec/react_on_rails/git_utils_spec.rb
@@ -6,7 +6,7 @@ module ReactOnRails
   RSpec.describe GitUtils do
     describe ".uncommitted_changes?" do
       context "with uncommited git changes" do
-        let(:message_handler) { instance_double(MessageHandler) }
+        let(:message_handler) { instance_double("MessageHandler") } # rubocop:disable RSpec/VerifiedDoubleReference
 
         it "returns true" do
           allow(described_class).to receive(:`).with("git status --porcelain").and_return("M file/path")
@@ -19,7 +19,7 @@ module ReactOnRails
       end
 
       context "with clean git status" do
-        let(:message_handler) { instance_double(MessageHandler) }
+        let(:message_handler) { instance_double("MessageHandler") } # rubocop:disable RSpec/VerifiedDoubleReference
 
         it "returns false" do
           allow(described_class).to receive(:`).with("git status --porcelain").and_return("")
@@ -31,7 +31,7 @@ module ReactOnRails
       end
 
       context "with git not installed" do
-        let(:message_handler) { instance_double(MessageHandler) }
+        let(:message_handler) { instance_double("MessageHandler") } # rubocop:disable RSpec/VerifiedDoubleReference
 
         it "returns true" do
           allow(described_class).to receive(:`).with("git status --porcelain").and_return(nil)
