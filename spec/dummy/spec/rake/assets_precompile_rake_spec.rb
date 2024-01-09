2ebe31eb525ca131642787cd4ce2755e8964a14d
Mostafa Ahangarha <ahangarha@riseup.net>
2024-01-10 00:34:36 +0330

    Add test for webpacker:clean deprecation message

---
+++ spec/dummy/spec/rake/assets_precompile_rake_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "rake"
+require "fileutils"
+
+require_relative "../rails_helper"
+
+describe "rake assets:precompile task" do
+  it "doesn't show deprecation message for using webpacker:clean task" do
+    ENV["SHAKAPACKER_PRECOMPILE"] = "false"
+
+    Rails.application.load_tasks
+
+    ReactOnRails.configure do |config|
+      config.build_production_command = "RAILS_ENV=production NODE_ENV=production bin/shakapacker"
+    end
+
+    expect do
+      Rake::Task["assets:precompile"].execute
+    end.not_to output(/Consider using `rake shakapacker:clean`/).to_stdout
+  end
+end
