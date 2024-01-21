c90ce5f3dd73a7050eab9298f7ea089eee522fa7
Mostafa Ahangarha <ahangarha@riseup.net>
2024-01-21 21:19:25 +0330

    Mock ENV[]

--- spec/dummy/spec/rake/assets_precompile_rake_spec.rb
+++ spec/dummy/spec/rake/assets_precompile_rake_spec.rb
@@ -6,7 +6,8 @@ require "rails_helper"
 
 describe "rake assets:precompile task" do
   it "doesn't show deprecation message for using webpacker:clean task" do
-    ENV["SHAKAPACKER_PRECOMPILE"] = "false"
+    allow(ENV).to receive(:[]).with(anything).and_call_original
+    allow(ENV).to receive(:[]).with("SHAKAPACKER_PRECOMPILE").and_return("false")
 
     Rails.application.load_tasks
 
