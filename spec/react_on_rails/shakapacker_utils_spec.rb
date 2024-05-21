cae7bfa6cc43198c7a045d0fd3caa241d7df402d
adriangohjw <adriangohjw@gmail.com>
2024-05-22 02:31:38 +0800

    update min. shakapacker version to 7.2.1

--- spec/react_on_rails/shakapacker_utils_spec.rb
+++ spec/react_on_rails/shakapacker_utils_spec.rb
@@ -13,33 +13,32 @@ module ReactOnRails
     end
 
     describe ".shackapacker_version_requirement_met?" do
-      minimum_version = [6, 5, 3]
+      minimum_version = [7, 2, 1]
 
       it "returns false when version is lower than minimum_version" do
-        allow(described_class).to receive(:shakapacker_version).and_return("6.5.0")
-
+        allow(described_class).to receive(:shakapacker_version).and_return("7.2.0")
         expect(described_class.shackapacker_version_requirement_met?(minimum_version)).to be(false)
 
-        allow(described_class).to receive(:shakapacker_version).and_return("6.4.7")
+        allow(described_class).to receive(:shakapacker_version).and_return("7.1.0")
         expect(described_class.shackapacker_version_requirement_met?(minimum_version)).to be(false)
 
-        allow(described_class).to receive(:shakapacker_version).and_return("5.7.7")
+        allow(described_class).to receive(:shakapacker_version).and_return("6.6.0")
         expect(described_class.shackapacker_version_requirement_met?(minimum_version)).to be(false)
       end
 
       it "returns true when version is equal to minimum_version" do
-        allow(described_class).to receive(:shakapacker_version).and_return("6.5.3")
+        allow(described_class).to receive(:shakapacker_version).and_return("7.2.1")
         expect(described_class.shackapacker_version_requirement_met?(minimum_version)).to be(true)
       end
 
       it "returns true when version is greater than minimum_version" do
-        allow(described_class).to receive(:shakapacker_version).and_return("6.6.0")
+        allow(described_class).to receive(:shakapacker_version).and_return("7.2.3")
         expect(described_class.shackapacker_version_requirement_met?(minimum_version)).to be(true)
 
-        allow(described_class).to receive(:shakapacker_version).and_return("6.5.4")
+        allow(described_class).to receive(:shakapacker_version).and_return("7.3.0")
         expect(described_class.shackapacker_version_requirement_met?(minimum_version)).to be(true)
 
-        allow(described_class).to receive(:shakapacker_version).and_return("7.7.7")
+        allow(described_class).to receive(:shakapacker_version).and_return("8.0.0")
         expect(described_class.shackapacker_version_requirement_met?(minimum_version)).to be(true)
       end
     end
