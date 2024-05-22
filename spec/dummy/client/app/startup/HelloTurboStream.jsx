a762887d22ab68da6426aec46c627a3154a362ba
theforestvn88 <theforestvn88@gmail.com>
2024-05-22 16:30:07 +0700

    fix review

--- spec/dummy/client/app/startup/HelloTurboStream.jsx
+++ spec/dummy/client/app/startup/HelloTurboStream.jsx
@@ -4,41 +4,27 @@ import RailsContext from '../components/RailsContext';
 
 import css from '../components/HelloWorld.module.scss';
 
-class HelloTurboStream extends React.Component {
-  static propTypes = {
-    helloTurboStreamData: PropTypes.shape({
-      name: PropTypes.string,
-    }).isRequired,
-    railsContext: PropTypes.object,
+const HelloTurboStream = ({ helloTurboStreamData, railsContext }) => {
+  const [name, setName] = useState(helloTurboStreamData.name);
+  const nameDomRef = useRef(null);
+
+  const handleChange = () => {
+    setName(nameDomRef.current.value);
   };
 
-  constructor(props) {
-    super(props);
-    this.state = props.helloTurboStreamData;
-    this.setNameDomRef = this.setNameDomRef.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
+  return (
+    <div>
+      <h3 className={css.brightColor}>Hello, {name}!</h3>
+      {railsContext && <RailsContext {...{ railsContext }} />}
+    </div>
+  );
+};
 
-  setNameDomRef(nameDomNode) {
-    this.nameDomRef = nameDomNode;
-  }
-
-  handleChange() {
-    const name = this.nameDomRef.value;
-    this.setState({ name });
-  }
-
-  render() {
-    const { name } = this.state;
-    const { railsContext } = this.props;
-
-    return (
-      <div>
-        <h3 className={css.brightColor}>Hello, {name}!</h3>
-        {railsContext && <RailsContext {...{ railsContext }} />}
-      </div>
-    );
-  }
-}
+HelloTurboStream.propTypes = {
+  helloTurboStreamData: PropTypes.shape({
+    name: PropTypes.string,
+  }).isRequired,
+  railsContext: PropTypes.object,
+};
 
 export default HelloTurboStream;
