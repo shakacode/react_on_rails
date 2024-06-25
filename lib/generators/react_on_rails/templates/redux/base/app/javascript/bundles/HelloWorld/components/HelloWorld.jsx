76c6b50d9814fda73c9eb33ef36fe869947573ed
Mikhail Boutylin <lahmataja.pa4vara@gmail.com>
2024-06-25 06:54:23 +0400

    fixed css-loader instalation and css import in example files (#1634)
    Related to https://github.com/webpack-contrib/css-loader/blob/master/CHANGELOG.md#-breaking-changes

--- lib/generators/react_on_rails/templates/redux/base/app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
+++ lib/generators/react_on_rails/templates/redux/base/app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import style from './HelloWorld.module.css';
+import * as style from './HelloWorld.module.css';
 
 const HelloWorld = ({ name, updateName }) => (
   <div>
