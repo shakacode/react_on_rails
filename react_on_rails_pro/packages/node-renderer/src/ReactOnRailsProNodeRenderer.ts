94544726f8125f3e5d29085223811c2ccbf769f8
Alexey Romanov <104450112+alexeyr-ci@users.noreply.github.com>
2024-12-02 17:38:31 +0300

    Default to Fastify 5 (#476)
    Upgrade Fastify and its plugins to v5. Since it doesn't work on Node
    versions before 20, we check for the incompatible versions explicitly
    and fail with an error message saying how to solve it.
    
    Fixes #475.
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **Chores**
    - Updated dependency version constraints for improved compatibility with
    newer versions of packages.
    - **New Features**
    - Introduced a new function to extract the major version number from a
    version string.
    - Added error handling for incompatible Node.js and Fastify versions in
    the rendering function.
    - Enhanced type definitions for better alignment with existing
    structures.
    - Overhauled error reporting and tracing integrations for improved error
    handling.
    - Upgraded Fastify to version 5, with an option to revert to version 4
    for compatibility.
    - Improved server rendering capabilities with enhanced error handling
    and support for replaying console logs.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: Alexey Romanov <alexey.v.romanov@gmail.com>

--- react_on_rails_pro/packages/node-renderer/src/ReactOnRailsProNodeRenderer.ts
+++ react_on_rails_pro/packages/node-renderer/src/ReactOnRailsProNodeRenderer.ts
@@ -1,12 +1,38 @@
 import cluster from 'cluster';
-import master from './master';
-import worker from './worker';
+import { version as fastifyVersion } from 'fastify/package.json';
 import { Config } from './shared/configBuilder';
+import log from './shared/log';
+import { majorVersion } from './shared/utils';
 
 export async function reactOnRailsProNodeRenderer(config: Partial<Config> = {}) {
-  if (cluster.isPrimary) {
-    master(config);
-  } else {
-    await worker(config).ready();
+  const fastify5Supported = majorVersion(process.versions.node) >= 20;
+  const fastify5OrNewer = majorVersion(fastifyVersion) >= 5;
+  if (fastify5OrNewer && !fastify5Supported) {
+    log.error(
+      `Node.js version ${process.versions.node} is not supported by Fastify ${fastifyVersion}.
+Please either use Node.js v20 or higher or downgrade Fastify by setting the following resolutions in your package.json:
+{
+  "@fastify/formbody": "^7.4.0",
+  "@fastify/multipart": "^8.3.0",
+  "fastify": "^4.28.1",
+}`,
+    );
+    process.exit(1);
+  } else if (!fastify5OrNewer && fastify5Supported) {
+    log.warn(
+      `Fastify 5+ supports Node.js ${process.versions.node}, but the current version of Fastify is ${fastifyVersion}.
+You have probably forced an older version of Fastify by adding resolutions for it
+and for "@fastify/..." dependencies in your package.json. Consider removing them.`,
+    );
   }
+
+  /* eslint-disable global-require,@typescript-eslint/no-var-requires --
+   * Using normal `import` fails before the check above.
+   */
+  if (cluster.isPrimary) {
+    (require('./master') as typeof import('./master'))(config);
+  } else {
+    await (require('./worker') as typeof import('./worker')).default(config).ready();
+  }
+  /* eslint-enable global-require,@typescript-eslint/no-var-requires */
 }
