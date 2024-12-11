1744fa045dfcef748c8f4f7a6070a84c812983fa
Abanoub Ghadban <abanoub@shakacode.com>
2024-12-11 19:54:53 +0200

    stream rsc payload in json objects like streamed react components

--- node_package/src/RSCClientRoot.ts
+++ node_package/src/RSCClientRoot.ts
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import RSDWClient from 'react-server-dom-webpack/client';
+import transformRSCStreamAndReplayConsoleLogs from './transformRSCStreamAndReplayConsoleLogs';
 
 if (!('use' in React)) {
   throw new Error('React.use is not defined. Please ensure you are using React 18 with experimental features enabled or React 19+ to use server components.');
@@ -14,9 +15,19 @@ export type RSCClientRootProps = {
   rscRenderingUrlPath: string;
 }
 
+const createFromFetch = async (fetchPromise: Promise<Response>) => {
+  const response = await fetchPromise;
+  const stream = response.body;
+  if (!stream) {
+    throw new Error('No stream found in response');
+  }
+  const transformedStream = transformRSCStreamAndReplayConsoleLogs(stream);
+  return RSDWClient.createFromReadableStream(transformedStream);
+}
+
 const fetchRSC = ({ componentName, rscRenderingUrlPath }: RSCClientRootProps) => {
   if (!renderCache[componentName]) {
-    renderCache[componentName] = RSDWClient.createFromFetch(fetch(`${rscRenderingUrlPath}/${componentName}`)) as Promise<React.ReactNode>;
+    renderCache[componentName] = createFromFetch(fetch(`${rscRenderingUrlPath}/${componentName}`)) as Promise<React.ReactNode>;
   }
   return renderCache[componentName];
 }
