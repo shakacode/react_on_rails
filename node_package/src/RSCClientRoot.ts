f0649cf3bfb456cd5008babc26406b4fa66ecb2c
Abanoub Ghadban <abanoub@shakacode.com>
2024-12-18 13:22:14 +0200

    refactoring

--- node_package/src/RSCClientRoot.ts
+++ node_package/src/RSCClientRoot.ts
@@ -2,18 +2,16 @@ import * as React from 'react';
 import RSDWClient from 'react-server-dom-webpack/client';
 
 if (!('use' in React)) {
-  throw new Error('React.use is not defined. Please ensure you are using React 18.3.0-canary-670811593-20240322 or later to use server components.');
+  throw new Error('React.use is not defined. Please ensure you are using React 18 with experimental features enabled or React 19+ to use server components.');
 }
 
-// It's not the exact type, but it's close enough for now
-type Use = <T>(promise: Promise<T>) => T;
-const { use } = React as { use: Use };
+const { use } = React;
 
-const renderCache: Record<string, Promise<unknown>> = {};
+const renderCache: Record<string, Promise<React.ReactNode>> = {};
 
 const fetchRSC = ({ componentName }: { componentName: string }) => {
   if (!renderCache[componentName]) {
-    renderCache[componentName] = RSDWClient.createFromFetch(fetch(`/rsc/${componentName}`));
+    renderCache[componentName] = RSDWClient.createFromFetch(fetch(`/rsc/${componentName}`)) as Promise<React.ReactNode>;
   }
   return renderCache[componentName];
 }
