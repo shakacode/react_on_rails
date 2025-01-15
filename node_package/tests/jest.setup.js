34acb2242e53d55f3f4a10b61a4bb6e8d4ffa547
Alexey Romanov <alexey.v.romanov@gmail.com>
2025-01-15 13:00:46 +0000

    Update React in root to v19

--- node_package/tests/jest.setup.js
+++ node_package/tests/jest.setup.js
@@ -5,9 +5,31 @@ if (typeof window !== 'undefined' && typeof window.TextEncoder !== 'undefined') 
   throw new Error('TextEncoder is already defined, remove the polyfill');
 }
 
+// Similarly for MessageChannel
+if (typeof window !== 'undefined' && typeof window.MessageChannel !== 'undefined') {
+  throw new Error('MessageChannel is already defined, remove the polyfill');
+}
+
 if (typeof window !== 'undefined') {
   // eslint-disable-next-line global-require
   const { TextEncoder, TextDecoder } = require('util');
   global.TextEncoder = TextEncoder;
   global.TextDecoder = TextDecoder;
+
+  // https://github.com/jsdom/jsdom/issues/2448#issuecomment-1703763542
+  global.MessageChannel = jest.fn().mockImplementation(() => {
+    let onmessage;
+    return {
+      port1: {
+        set onmessage(cb) {
+          onmessage = cb;
+        },
+      },
+      port2: {
+        postMessage: data => {
+          onmessage?.({ data });
+        },
+      },
+    };
+  });
 }
