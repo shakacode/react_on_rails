032dcbfe5eb456885c988289dd172fca2aed8df7
Alexey Romanov <alexey.v.romanov@gmail.com>
2025-01-23 14:30:24 +0000

    Detect dead code in spec/dummy as well

--- knip.ts
+++ knip.ts
@@ -2,25 +2,56 @@ import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
   // ! at the end means files are used in production
-  entry: ['node_package/src/ReactOnRails.ts!', 'node_package/src/ReactOnRails.node.ts!'],
-  project: ['node_package/src/**/*.[jt]s!', 'node_package/tests/**/*.[jt]s'],
-  ignoreBinaries: [
-    // Knip fails to detect it's declared in devDependencies
-    'nps',
-    // local scripts
-    'node_package/scripts/.*',
-  ],
-  ignoreDependencies: [
-    // Required for TypeScript compilation, but we don't depend on Turbolinks itself.
-    '@types/turbolinks',
-    // used in package-scripts.yml
-    'concurrently',
-    // The Knip ESLint plugin fails to detect it's transitively required by a config,
-    // though we don't actually use its rules anywhere.
-    'eslint-plugin-jsx-a11y',
-  ],
-  babel: {
-    config: ['node_package/babel.config.js', 'package.json'],
+  workspaces: {
+    '.': {
+      entry: ['node_package/src/ReactOnRails.ts!', 'node_package/src/ReactOnRails.node.ts!'],
+      project: ['node_package/src/**/*.[jt]s!', 'node_package/tests/**/*.[jt]s'],
+      ignoreBinaries: [
+        // Knip fails to detect it's declared in devDependencies
+        'nps',
+        // local scripts
+        'node_package/scripts/.*',
+      ],
+      ignoreDependencies: [
+        // Required for TypeScript compilation, but we don't depend on Turbolinks itself.
+        '@types/turbolinks',
+        // used in package-scripts.yml
+        'concurrently',
+        // The Knip ESLint plugin fails to detect it's transitively required by a config,
+        // though we don't actually use its rules anywhere.
+        'eslint-plugin-jsx-a11y',
+      ],
+      babel: {
+        config: ['node_package/babel.config.js'],
+      },
+    },
+    'spec/dummy': {
+      entry: [
+        'app/assets/config/manifest.js!',
+        'client/app/packs/**/*.js!',
+        // Not sure why this isn't detected as a dependency of client/app/packs/server-bundle.js
+        'client/app/generated/server-bundle-generated.js!',
+        'spec/fixtures/automated_packs_generation/**/*.js{x,}',
+        'config/webpack/{production,development,test}.js',
+        // Declaring this as webpack.config instead doesn't work correctly
+        'config/webpack/webpack.config.js',
+      ],
+      paths: {
+        'Assets/*': ['client/app/assets/*'],
+      },
+      ignoreBinaries: [
+        // Has to be installed globally
+        'yalc',
+        // Local binaries
+        'bin/.*',
+      ],
+      ignoreDependencies: [
+        // There's no ReScript plugin for Knip
+        '@rescript/react',
+        // The Babel plugin fails to detect it
+        'babel-plugin-transform-react-remove-prop-types',
+      ],
+    },
   },
 };
 
