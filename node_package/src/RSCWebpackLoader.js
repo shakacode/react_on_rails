9ea28f4694556ebc1e9da14eacf95fa93950207b
Abanoub Ghadban <abanoub@shakacode.com>
2025-01-25 16:04:49 +0200

    linting

--- node_package/src/RSCWebpackLoader.js
+++ node_package/src/RSCWebpackLoader.js
@@ -3,17 +3,18 @@ const { pathToFileURL } = require('url');
 const RSCWebpackLoader = async function Loader(source, sourceMap) {
   // Mark loader as async since we're doing async operations
   const callback = this.async();
-  
+
   try {
     // Convert file path to URL format
     const fileUrl = pathToFileURL(this.resourcePath).href;
 
+    // eslint-disable-next-line import/no-unresolved
     const { load } = await import('react-server-dom-webpack/node-loader');
     const result = await load(fileUrl, null, async () => ({
       format: 'module',
       source,
     }));
-    
+
     callback(null, result.source, sourceMap);
   } catch (error) {
     callback(error);
