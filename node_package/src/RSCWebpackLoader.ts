d517143e6b18b8384c7eed6f918c296f9105ce20
Alexey Romanov <alexey.v.romanov@gmail.com>
2025-02-10 09:32:18 +0000

    Comment on the need for workaround

--- node_package/src/RSCWebpackLoader.ts
+++ node_package/src/RSCWebpackLoader.ts
@@ -9,6 +9,10 @@ const RSCWebpackLoader: LoaderDefinition = function RSCWebpackLoader(source, sou
     // Convert file path to URL format
     const fileUrl = pathToFileURL(this.resourcePath).href;
 
+    // Workaround for TS transpiling `await import` while we need to keep it.
+    // See https://github.com/microsoft/TypeScript/issues/43329#issuecomment-1008361973
+    // If we end up needing it more than once, prefer creating a non-compiled
+    // `dynamicImport.js` file instead.
     // eslint-disable-next-line no-new-func
     const { load } = await new Function('return import("react-server-dom-webpack/node-loader")')() as
       typeof import('react-server-dom-webpack/node-loader');
