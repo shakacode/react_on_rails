4ac1fe1bc5e603b0f14fe37aac20926677fe13b9
Alexey Romanov <alexey.v.romanov@gmail.com>
2025-02-10 20:32:45 +0000

    Simplify RSCWebpackLoader

--- node_package/src/RSCWebpackLoader.ts
+++ node_package/src/RSCWebpackLoader.ts
@@ -1,29 +1,22 @@
 import { pathToFileURL } from 'url';
 import { LoaderDefinition } from 'webpack';
 
-const RSCWebpackLoader: LoaderDefinition = function RSCWebpackLoader(source, sourceMap) {
-  // Mark loader as async since we're doing async operations
-  const callback = this.async();
+const RSCWebpackLoader: LoaderDefinition = async function RSCWebpackLoader(source) {
+  // Convert file path to URL format
+  const fileUrl = pathToFileURL(this.resourcePath).href;
 
-  (async () => {
-    // Convert file path to URL format
-    const fileUrl = pathToFileURL(this.resourcePath).href;
-
-    // Workaround for TS transpiling `await import` while we need to keep it.
-    // See https://github.com/microsoft/TypeScript/issues/43329#issuecomment-1008361973
-    // If we end up needing it more than once, prefer creating a non-compiled
-    // `dynamicImport.js` file instead.
-    // eslint-disable-next-line no-new-func
-    const { load } = await new Function('return import("react-server-dom-webpack/node-loader")')() as
-      typeof import('react-server-dom-webpack/node-loader');
-    return load(fileUrl, null, async () => ({
-      format: 'module',
-      source,
-    }));
-  })().then(
-    result => callback(null, result.source, sourceMap),
-    error => callback(error),
-  );
+  // Workaround for TS transpiling `await import` while we need to keep it.
+  // See https://github.com/microsoft/TypeScript/issues/43329#issuecomment-1008361973
+  // If we end up needing it more than once, prefer creating a non-compiled
+  // `dynamicImport.js` file instead.
+  // eslint-disable-next-line no-new-func
+  const { load } = await new Function('return import("react-server-dom-webpack/node-loader")')() as
+    typeof import('react-server-dom-webpack/node-loader');
+  const result = await load(fileUrl, null, async () => ({
+    format: 'module',
+    source,
+  }));
+  return result.source;
 };
 
 export default RSCWebpackLoader;
