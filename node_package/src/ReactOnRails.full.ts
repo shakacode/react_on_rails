7d45d414c558d8c3e25973c84be7736e9b0ded36
Roman Kuksin <roman@shakacode.com>
2025-02-12 19:57:54 +0400

    nits

--- node_package/src/ReactOnRails.full.ts
+++ node_package/src/ReactOnRails.full.ts
@@ -9,10 +9,19 @@ import type {
 import Client from './ReactOnRails.client';
 
 if (typeof window !== 'undefined') {
-  console.warn('Optimization opportunity: "react-on-rails" includes server-rendering code. Browser may not need it. See https://forum.shakacode.com/t/how-to-use-different-versions-of-a-file-for-client-and-server-rendering/1352 (requires sign in)');
+  console.log('ReactOnRails: Optimization opportunity: "react-on-rails" includes server-rendering code. Browser may not need it. See https://forum.shakacode.com/t/how-to-use-different-versions-of-a-file-for-client-and-server-rendering/1352 (requires sign in)');
 }
 
+/**
+ * Used by Rails to catch errors in rendering
+ * @param options
+ */
 Client.handleError = (options: ErrorOptions): string | undefined => handleError(options);
+
+/**
+ * Used by server rendering by Rails
+ * @param options
+ */
 Client.serverRenderReactComponent = (options: RenderParams): null | string | Promise<RenderResult> => serverRenderReactComponent(options);
 
 export * from "./types";
