5c36388bac363f7ea889dfe1e9884239eb9f57b7
Abanoub Ghadban <abanoub@shakacode.com>
2025-02-20 18:15:05 +0200

    pass props to RSC generator and avoid state reset on hydration

--- jest.config.js
+++ jest.config.js
@@ -6,11 +6,12 @@ module.exports = {
   setupFiles: ['<rootDir>/node_package/tests/jest.setup.js'],
   // React Server Components tests are not compatible with Experimental React 18 and React 19
   // That only run with node version 18 and above
-  moduleNameMapper: nodeVersion < 18
-    ? {
-        'react-server-dom-webpack/client': '<rootDir>/node_package/tests/emptyForTesting.js',
-        '^@testing-library/dom$': '<rootDir>/node_package/tests/emptyForTesting.js',
-        '^@testing-library/react$': '<rootDir>/node_package/tests/emptyForTesting.js',
-      }
-    : {},
+  moduleNameMapper:
+    nodeVersion < 18
+      ? {
+          'react-server-dom-webpack/client': '<rootDir>/node_package/tests/emptyForTesting.js',
+          '^@testing-library/dom$': '<rootDir>/node_package/tests/emptyForTesting.js',
+          '^@testing-library/react$': '<rootDir>/node_package/tests/emptyForTesting.js',
+        }
+      : {},
 };
