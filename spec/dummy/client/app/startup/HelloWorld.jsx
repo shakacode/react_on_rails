d962cb318f2860a6839faa0691bdb075459acd0f
Alexey Romanov <alexey.v.romanov@gmail.com>
2025-03-15 09:24:34 +0000

    Auto-fixes

--- spec/dummy/client/app/startup/HelloWorld.jsx
+++ spec/dummy/client/app/startup/HelloWorld.jsx
@@ -32,7 +32,6 @@ class HelloWorld extends React.Component {
   }
 
   render() {
-    // eslint-disable-next-line no-console
     console.log(
       'HelloWorld demonstrating a call to console.log in ' +
         'spec/dummy/client/app/components/HelloWorld.jsx:18',
