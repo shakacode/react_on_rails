3b956375ccd86ae1841032e3a67c5d6ae011742d
Abanoub Ghadban <abanoub@shakacode.com>
2025-03-26 18:04:51 +0200

    embed rsc payload inside the html page

--- node_package/src/buildConsoleReplay.ts
+++ node_package/src/buildConsoleReplay.ts
@@ -51,11 +51,12 @@ export function consoleReplay(
 }
 
 export default function buildConsoleReplay(
-  customConsoleHistory: (typeof console)['history'] | undefined = undefined,
+  customConsoleHistory: typeof console['history'] | undefined = undefined,
   numberOfMessagesToSkip: number = 0,
 ): string {
-  return RenderUtils.wrapInScriptTags(
-    'consoleReplayLog',
-    consoleReplay(customConsoleHistory, numberOfMessagesToSkip),
-  );
+  const consoleReplayJS = consoleReplay(customConsoleHistory, numberOfMessagesToSkip);
+  if (consoleReplayJS.length === 0) {
+    return '';
+  }
+  return RenderUtils.wrapInScriptTags('consoleReplayLog', consoleReplayJS);
 }
