24cbdd5ec8813c4bbf7bd8f3cec1dac7e64ee88a
Abanoub Ghadban <abanoub@shakacode.com>
2025-03-27 13:10:38 +0200

    linting

--- node_package/src/RSCServerRoot.ts
+++ node_package/src/RSCServerRoot.ts
@@ -17,20 +17,28 @@ declare global {
 type RSCServerRootProps = {
   componentName: string;
   componentProps: Record<string, unknown>;
-}
+};
 
 if (!('use' in React && typeof React.use === 'function')) {
-  throw new Error('React.use is not defined. Please ensure you are using React 18 with experimental features enabled or React 19+ to use server components.');
+  throw new Error(
+    'React.use is not defined. Please ensure you are using React 18 with experimental features enabled or React 19+ to use server components.',
+  );
 }
 
 const { use } = React;
 
-const createFromReactOnRailsNodeStream = (stream: NodeJS.ReadableStream, ssrManifest: Record<string, unknown>) => {
+const createFromReactOnRailsNodeStream = (
+  stream: NodeJS.ReadableStream,
+  ssrManifest: Record<string, unknown>,
+) => {
   const transformedStream = transformRSCStream(stream);
   return createFromNodeStream(transformedStream, ssrManifest);
-}
+};
 
-const createSSRManifest = async (reactServerManifestFileName: string, reactClientManifestFileName: string) => {
+const createSSRManifest = async (
+  reactServerManifestFileName: string,
+  reactClientManifestFileName: string,
+) => {
   const [reactServerManifest, reactClientManifest] = await Promise.all([
     loadJsonFile(reactServerManifestFileName),
     loadJsonFile(reactClientManifestFileName),
@@ -38,7 +46,7 @@ const createSSRManifest = async (reactServerManifestFileName: string, reactClien
 
   const ssrManifest = {
     moduleLoading: {
-      prefix: "/webpack/development/",
+      prefix: '/webpack/development/',
       crossOrigin: null,
     },
     moduleMap: {} as Record<string, unknown>,
@@ -51,38 +59,47 @@ const createSSRManifest = async (reactServerManifestFileName: string, reactClien
         id: (serverFileBundlingInfo as { id: string }).id,
         chunks: (serverFileBundlingInfo as { chunks: string[] }).chunks,
         name: '*',
-      }
+      },
     };
   });
 
   return ssrManifest;
-}
+};
 
-const RSCServerRoot: RenderFunction = async ({ componentName, componentProps }: RSCServerRootProps, railsContext?: RailsContext) => {
-  if (!railsContext?.serverSide || !railsContext?.reactClientManifestFileName || !railsContext?.reactServerClientManifestFileName) {
+const RSCServerRoot: RenderFunction = async (
+  { componentName, componentProps }: RSCServerRootProps,
+  railsContext?: RailsContext,
+) => {
+  if (
+    !railsContext?.serverSide ||
+    !railsContext?.reactClientManifestFileName ||
+    !railsContext?.reactServerClientManifestFileName
+  ) {
     throw new Error(
-      `${'serverClientManifestFileName and reactServerClientManifestFileName are required. ' +
-      'Please ensure that React Server Component webpack configurations are properly set ' +
-      'as stated in the React Server Component tutorial. The received rails context is: '}${  JSON.stringify(railsContext)}`
+      `${
+        'serverClientManifestFileName and reactServerClientManifestFileName are required. ' +
+        'Please ensure that React Server Component webpack configurations are properly set ' +
+        'as stated in the React Server Component tutorial. The received rails context is: '
+      }${JSON.stringify(railsContext)}`,
     );
   }
 
   if (typeof generateRSCPayload !== 'function') {
     throw new Error(
       'generateRSCPayload is not defined. Please ensure that you are using at least version 4.0.0 of ' +
-      'React on Rails Pro and the node renderer, and that ReactOnRailsPro.configuration.enable_rsc_support ' +
-      'is set to true.'
+        'React on Rails Pro and the node renderer, and that ReactOnRailsPro.configuration.enable_rsc_support ' +
+        'is set to true.',
     );
   }
 
   const ssrManifest = await createSSRManifest(
     railsContext.reactServerClientManifestFileName,
-    railsContext.reactClientManifestFileName
+    railsContext.reactClientManifestFileName,
   );
   const rscPayloadStream = await generateRSCPayload(
     componentName,
     componentProps,
-    railsContext.serverSideRSCPayloadParameters
+    railsContext.serverSideRSCPayloadParameters,
   );
 
   // Tee the stream to pass it to the server component and the payload container
@@ -92,20 +109,14 @@ const RSCServerRoot: RenderFunction = async ({ componentName, componentProps }: 
   rscPayloadStream.pipe(rscPayloadStream2);
   const serverComponentElement = createFromReactOnRailsNodeStream(rscPayloadStream1, ssrManifest);
 
-  return () => React.createElement(
-    React.Fragment,
-    null, [
-      React.createElement(
-        React.Fragment,
-        { key: 'serverComponentElement' },
-        use(serverComponentElement)
-      ),
-      React.createElement(
-        RSCPayloadContainer,
-        { RSCPayloadStream: rscPayloadStream2, key: 'rscPayloadContainer' },
-      ),
-    ]
-  );
+  return () =>
+    React.createElement(React.Fragment, null, [
+      React.createElement(React.Fragment, { key: 'serverComponentElement' }, use(serverComponentElement)),
+      React.createElement(RSCPayloadContainer, {
+        RSCPayloadStream: rscPayloadStream2,
+        key: 'rscPayloadContainer',
+      }),
+    ]);
 };
 
 export default RSCServerRoot;
