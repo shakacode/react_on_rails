620ff943b0861e3ccf67667157f441484edc0dfc
Abanoub Ghadban <abanoub@shakacode.com>
2025-03-31 13:24:56 +0200

    Add usage guide for React on Rails render functions and tests for serverRenderReactComponent

--- node_package/tests/serverRenderReactComponent.test.ts
+++ node_package/tests/serverRenderReactComponent.test.ts
@@ -81,7 +81,7 @@ describe('serverRenderReactComponent', () => {
     expect(hasErrors).toBeTruthy();
   });
 
-  it('serverRenderReactComponent renders html', () => {
+  it('serverRenderReactComponent renders html renderedHtml property', () => {
     const expectedHtml = '<div>Hello</div>';
     const X3: RenderFunction = (_: unknown, __?: RailsContext): { renderedHtml: string } => ({
       renderedHtml: expectedHtml,
@@ -103,6 +103,133 @@ describe('serverRenderReactComponent', () => {
 
     expect(html).toEqual(expectedHtml);
     expect(hasErrors).toBeFalsy();
+  });
+
+  it("doesn't render object without renderedHtml property", () => {
+    const X4 = (_props: unknown, _railsContext?: RailsContext): { foo: string } => ({
+      foo: 'bar',
+    });
+
+    ComponentRegistry.register({ X4: X4 as unknown as RenderFunction });
+    const renderResult = serverRenderReactComponent({
+      name: 'X4',
+      domNodeId: 'myDomId',
+      trace: false,
+      throwJsErrors: false,
+      renderingReturnsPromises: false,
+    });
+    assertIsString(renderResult);
+    const { html, hasErrors }: RenderResult = JSON.parse(renderResult) as RenderResult;
+
+    expect(html).toContain('Exception in rendering!');
+    expect(html).toContain('Element type is invalid');
+    expect(hasErrors).toBeTruthy();
+  });
+
+  // If a render function returns a string, serverRenderReactComponent will interpret it as a React component type.
+  // For example, if the render function returns the string 'div', it will be treated as if React.createElement('div', props)
+  // was called. Thus, calling `serverRenderReactComponent({ name: 'X4', props: { foo: 'bar' } })` with a render function
+  // that returns 'div' will result in the HTML `<div foo="bar"></div>`.
+  // This behavior is an unintended side effect of the implementation.
+  // If the render function returns a real HTML string, it will most likely throw an invalid tag React error.
+  // For example, calling `serverRenderReactComponent({ name: 'X4', props: { foo: 'bar' } })` with a render function
+  // that returns '<div>Hello</div>' will result in the error:
+  // "Error: Invalid tag name <div>Hello</div>"
+  it("doesn't render html string returned directly from render function", () => {
+    const expectedHtml = '<div>Hello</div>';
+    const X4: RenderFunction = (_props: unknown, _railsContext?: RailsContext): string => expectedHtml;
+
+    ComponentRegistry.register({ X4 });
+
+    const renderParams: RenderParams = {
+      name: 'X4',
+      domNodeId: 'myDomId',
+      trace: false,
+      throwJsErrors: false,
+      renderingReturnsPromises: false,
+    };
+
+    const renderResult = serverRenderReactComponent(renderParams);
+    assertIsString(renderResult);
+    const { html, hasErrors }: RenderResult = JSON.parse(renderResult) as RenderResult;
+
+    // Instead of expecting exact strings, check that it contains key parts of the error
+    expect(html).toContain('Exception in rendering!');
+    expect(html).toContain('Invalid tag');
+    expect(html).toContain('div&gt;Hello&lt;/div');
+    expect(hasErrors).toBeTruthy();
+  });
+
+  it('serverRenderReactComponent renders promise of string html', async () => {
+    const expectedHtml = '<div>Hello</div>';
+    const X5: RenderFunction = (_props: unknown, _railsContext?: RailsContext): Promise<string> =>
+      Promise.resolve(expectedHtml);
+
+    ComponentRegistry.register({ X5 });
+
+    const renderParams: RenderParams = {
+      name: 'X5',
+      domNodeId: 'myDomId',
+      trace: false,
+      throwJsErrors: false,
+      renderingReturnsPromises: true,
+    };
+
+    const renderResult = serverRenderReactComponent(renderParams);
+    assertIsPromise(renderResult);
+    const html = await renderResult.then((r) => r.html);
+
+    expect(html).toEqual(expectedHtml);
+    await expect(renderResult.then((r) => r.hasErrors)).resolves.toBeFalsy();
+  });
+
+  // When an async render function returns an object, serverRenderReactComponent will return the object as is.
+  // It does not validate properties like renderedHtml or hasErrors; it simply returns the object.
+  // This behavior can cause issues with the ruby_on_rails gem.
+  // To avoid such issues, ensure that the returned object includes a `componentHtml` property and use the `react_component_hash` helper.
+  // This is demonstrated in the "can render async render function used with react_component_hash helper" test.
+  it('serverRenderReactComponent returns the object returned by the async render function', async () => {
+    const expectedHtml = '<div>Hello</div>';
+    const X6 = ((_props: unknown, _railsContext?: RailsContext): Promise<ServerRenderResult> =>
+      Promise.resolve({ renderedHtml: expectedHtml })) as RenderFunction;
+
+    ComponentRegistry.register({ X6 });
+
+    const renderParams: RenderParams = {
+      name: 'X6',
+      domNodeId: 'myDomId',
+      trace: false,
+      throwJsErrors: false,
+      renderingReturnsPromises: true,
+    };
+
+    const renderResult = serverRenderReactComponent(renderParams);
+    assertIsPromise(renderResult);
+    const html = (await renderResult.then((r) => r.html)) as ServerRenderResult;
+    expectMatchObject(html, { renderedHtml: expectedHtml });
+  });
+
+  // Because the object returned by the async render function is returned as it is,
+  // we can make the async render function returns an object with `componentHtml` property.
+  // This is useful when we want to render a component using the `react_component_hash` helper.
+  it('can render async render function used with react_component_hash helper', async () => {
+    const X7 = (_props: unknown, _railsContext?: RailsContext) =>
+      Promise.resolve({ componentHtml: '<div>Hello</div>' });
+
+    ComponentRegistry.register({ X7: X7 as unknown as RenderFunction });
+
+    const renderParams: RenderParams = {
+      name: 'X7',
+      domNodeId: 'myDomId',
+      trace: false,
+      throwJsErrors: false,
+      renderingReturnsPromises: true,
+    };
+
+    const renderResult = serverRenderReactComponent(renderParams);
+    assertIsPromise(renderResult);
+    const result = await renderResult;
+    expect(result.html).toMatchObject({ componentHtml: '<div>Hello</div>' });
   });
 
   it('serverRenderReactComponent renders an error if attempting to render a renderer', () => {
@@ -129,49 +256,5 @@ describe('serverRenderReactComponent', () => {
     const result = html && html.indexOf('renderer') > 0 && html.indexOf('Exception in rendering!') > 0;
     expect(result).toBeTruthy();
     expect(hasErrors).toBeTruthy();
-  });
-
-  it('serverRenderReactComponent renders promises', async () => {
-    const expectedHtml = '<div>Hello</div>';
-    const X5: RenderFunction = (_props: unknown, _railsContext?: RailsContext): Promise<string> =>
-      Promise.resolve(expectedHtml);
-
-    ComponentRegistry.register({ X5 });
-
-    const renderParams: RenderParams = {
-      name: 'X5',
-      domNodeId: 'myDomId',
-      trace: false,
-      throwJsErrors: false,
-      renderingReturnsPromises: true,
-    };
-
-    const renderResult = serverRenderReactComponent(renderParams);
-    assertIsPromise(renderResult);
-    const html = await renderResult.then((r) => r.html);
-
-    expect(html).toEqual(expectedHtml);
-    await expect(renderResult.then((r) => r.hasErrors)).resolves.toBeFalsy();
-  });
-
-  it('serverRenderReactComponent renders promise of server side hash result', async () => {
-    const expectedHtml = '<div>Hello</div>';
-    const X6 = ((_props: unknown, _railsContext?: RailsContext): Promise<ServerRenderResult> =>
-      Promise.resolve({ renderedHtml: expectedHtml })) as RenderFunction;
-
-    ComponentRegistry.register({ X6 });
-
-    const renderParams: RenderParams = {
-      name: 'X6',
-      domNodeId: 'myDomId',
-      trace: false,
-      throwJsErrors: false,
-      renderingReturnsPromises: true,
-    };
-
-    const renderResult = serverRenderReactComponent(renderParams);
-    assertIsPromise(renderResult);
-    const html = (await renderResult.then((r) => r.html)) as ServerRenderResult;
-    expectMatchObject(html, { renderedHtml: expectedHtml });
   });
 });
