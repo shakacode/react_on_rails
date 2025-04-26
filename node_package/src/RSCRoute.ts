0ded299e8db10225828a3ba6d0ee096def20a01c
Abanoub Ghadban <abanoub@shakacode.com>
2025-04-26 11:48:27 +0300

    Revert "add artifical delay amd log statements"
    This reverts commit cd57337469aecd297d5a75ec69d6ab542a8656fd.

--- node_package/src/RSCRoute.ts
+++ node_package/src/RSCRoute.ts
@@ -7,20 +7,10 @@ export type RSCRouteProps = {
 };
 
 const RSCRoute = ({ componentName, componentProps }: RSCRouteProps) => {
-  React.useEffect(() => {
-    console.log('RSCRoute mounted for', componentName, '[DEBUG RSC]');
-
-    return () => {
-      console.log('RSCRoute unmounted for', componentName, '[DEBUG RSC]');
-    };
-  }, [componentName]);
-
   const { getComponent, getCachedComponent } = useRSC();
   let component = getCachedComponent(componentName, componentProps);
   if (!component) {
-    console.log(`RSCRoute component ${componentName} not found, fetching`, '[DEBUG RSC]');
     component = React.use(getComponent(componentName, componentProps));
-    console.log(`RSCRoute component ${componentName} fetched`, '[DEBUG RSC]');
   }
   return component;
 };
