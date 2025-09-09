0da52cb4587a8301d29976fca1b1f4d4c536a25c
ihabadham <ihabadham4p@gmail.com>
2025-09-09 20:44:47 +0300

    generator: adopt example bin/dev in generator and dummy app

--- spec/react_on_rails/binstubs/dev_spec.rb
+++ spec/react_on_rails/binstubs/dev_spec.rb
@@ -3,19 +3,91 @@
 RSpec.describe "bin/dev script" do
   let(:script_path) { "lib/generators/react_on_rails/bin/dev" }
 
-  it "loads without syntax errors" do
-    # Clear ARGV to avoid script execution
-    original_argv = ARGV.dup
-    ARGV.clear
-    ARGV << "help" # Use help mode to avoid external dependencies
+  # To suppress stdout during tests
+  original_stderr = $stderr
+  original_stdout = $stdout
+  before(:all) do
+    $stderr = File.open(File::NULL, "w")
+    $stdout = File.open(File::NULL, "w")
+  end
 
-    # Suppress output
-    allow_any_instance_of(Kernel).to receive(:puts)
+  after(:all) do
+    $stderr = original_stderr
+    $stdout = original_stdout
+  end
 
-    expect { load script_path }.not_to raise_error
+  it "includes pack generation function" do
+    script_content = File.read(script_path)
+    expect(script_content).to include("def generate_packs")
+    expect(script_content).to include("bundle exec rake react_on_rails:generate_packs")
+  end
 
-    # Restore original ARGV
-    ARGV.clear
-    ARGV.concat(original_argv)
+  it "supports static development mode" do
+    script_content = File.read(script_path)
+    expect(script_content).to include("run_static_development")
+    expect(script_content).to include("Procfile.dev-static")
+  end
+
+  it "supports production-like mode" do
+    script_content = File.read(script_path)
+    expect(script_content).to include("run_production_like")
+    expect(script_content).to include("RAILS_ENV=production NODE_ENV=production bundle exec rails assets:precompile")
+    expect(script_content).to include("rails server -p 3001")
+  end
+
+  it "supports help command" do
+    script_content = File.read(script_path)
+    expect(script_content).to include('ARGV[0] == "help" || ARGV[0] == "--help" || ARGV[0] == "-h"')
+    expect(script_content).to include("Usage: bin/dev [command]")
+  end
+
+  it "with Overmind installed, uses Overmind" do
+    allow(IO).to receive(:popen).with("overmind -v").and_return("Some truthy result")
+
+    expect_any_instance_of(Kernel).to receive(:system).with("bundle exec rake react_on_rails:generate_packs").and_return(true)
+    expect_any_instance_of(Kernel).to receive(:system).with("overmind start -f Procfile.dev")
+
+    load script_path
+  end
+
+  it "without Overmind and with Foreman installed, uses Foreman" do
+    allow(IO).to receive(:popen).with("overmind -v").and_raise(Errno::ENOENT)
+    allow(IO).to receive(:popen).with("foreman -v").and_return("Some truthy result")
+
+    expect_any_instance_of(Kernel).to receive(:system).with("bundle exec rake react_on_rails:generate_packs").and_return(true)
+    expect_any_instance_of(Kernel).to receive(:system).with("foreman start -f Procfile.dev")
+
+    load script_path
+  end
+
+  it "without Overmind and Foreman installed, exits with error message" do
+    allow(IO).to receive(:popen).with("overmind -v").and_raise(Errno::ENOENT)
+    allow(IO).to receive(:popen).with("foreman -v").and_raise(Errno::ENOENT)
+    allow_any_instance_of(Kernel).to receive(:exit!)
+
+    expected_message = <<~MSG
+      NOTICE:
+      For this script to run, you need either 'overmind' or 'foreman' installed on your machine. Please try this script after installing one of them.
+    MSG
+
+    expect { load script_path }.to output(expected_message).to_stderr_from_any_process
+  end
+
+  it "With Overmind and without Procfile, exits with error message" do
+    allow(IO).to receive(:popen).with("overmind -v").and_return("Some truthy result")
+
+    allow_any_instance_of(Kernel).to receive(:system).with("bundle exec rake react_on_rails:generate_packs").and_return(true)
+    allow_any_instance_of(Kernel)
+      .to receive(:system)
+      .with("overmind start -f Procfile.dev")
+      .and_raise(Errno::ENOENT)
+    allow_any_instance_of(Kernel).to receive(:exit!)
+
+    expected_message = <<~MSG
+      ERROR:
+      Please ensure `Procfile.dev` exists in your project!
+    MSG
+
+    expect { load script_path }.to output(expected_message).to_stderr_from_any_process
   end
 end
