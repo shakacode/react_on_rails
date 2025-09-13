cb6475c4fa8d28342a64e6a7aae729cbe3f6ac29
ihabadham <ihabadham4p@gmail.com>
2025-09-13 22:25:22 +0300

    fix eslint error because of using any type

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,7 @@
 import path from 'node:path';
 import { globalIgnores } from 'eslint/config';
 import jest from 'eslint-plugin-jest';
+import type { ESLint } from 'eslint';
 import licenseHeader from 'eslint-plugin-license-header';
 import prettierRecommended from 'eslint-plugin-prettier/recommended';
 import testingLibrary from 'eslint-plugin-testing-library';
@@ -52,20 +53,14 @@ const config = tsEslint.config([
   js.configs.recommended,
   compat.extends('eslint-config-shakacode'),
   {
-    files: [
-      'node_package/src/**/*.ts',
-      'node_package/src/**/*.tsx',
-    ],
+    files: ['node_package/src/**/*.ts', 'node_package/src/**/*.tsx'],
     plugins: {
-      'license-header': licenseHeader as any,
+      'license-header': licenseHeader as ESLint.Plugin,
     },
     rules: {
       'license-header/header': [
         'error',
-        [
-          '/* Copyright (c) 2015–2025 ShakaCode, LLC',
-          '   SPDX-License-Identifier: MIT */',
-        ],
+        ['/* Copyright (c) 2015–2025 ShakaCode, LLC', '   SPDX-License-Identifier: MIT */'],
       ],
     },
   },
