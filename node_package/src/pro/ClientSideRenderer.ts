c4a57a16ad96e7ea41f5cdb2a3341a2ca11f5105
Abanoub Ghadban <abanoub@shakacode.com>
2025-09-18 23:32:05 +0300

    Add proprietary licensing information to multiple Pro files
    - Introduced a proprietary license notice to various files in the Pro directory, clarifying that these files are not licensed under the MIT license and are part of React on Rails Pro.
    - This update enhances the legal protection of the codebase and ensures compliance with proprietary usage guidelines.

--- node_package/src/pro/ClientSideRenderer.ts
+++ node_package/src/pro/ClientSideRenderer.ts
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2025 Shakacode
+ *
+ * This file, and all other files in this directory, are NOT licensed under the MIT license.
+ *
+ * This file is part of React on Rails Pro.
+ *
+ * Unauthorized copying, modification, distribution, or use of this file, via any medium,
+ * is strictly prohibited. It is proprietary and confidential.
+ *
+ * For the full license agreement, see:
+ * https://github.com/shakacode/react_on_rails/blob/master/REACT-ON-RAILS-PRO-LICENSE.md
+ */
+
 /* eslint-disable max-classes-per-file */
 
 import type { ReactElement } from 'react';
