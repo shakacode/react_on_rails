shakacode/react_on_rails_pro

Refactor VM handling and introduce ExecutionContext
- Replaced the `runInVM` function with a new `ExecutionContext` class to manage VM contexts more effectively.
- Updated the `handleRenderRequest` function to utilize the new `ExecutionContext`, improving the handling of rendering requests.
- Enhanced error management by introducing `VMContextNotFoundError` for better clarity when VM contexts are missing.
- Refactored tests to align with the new execution context structure, ensuring consistent behavior across rendering scenarios.

packages/node-renderer/tests/serverRenderRSCReactComponent.test.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import { Readable } from 'stream';
-import { buildVM, getVMContext, resetVM } from '../src/worker/vm';
+import { buildExecutionContext, resetVM } from '../src/worker/vm';
 import { getConfig } from '../src/shared/configBuilder';
 
 const SimpleWorkingComponent = () => 'hello';
@@ -33,7 +33,8 @@ describe('serverRenderRSCReactComponent', () => {
   const getReactOnRailsRSCObject = async () => {
     const testBundlesDirectory = path.join(__dirname, '../../../spec/dummy/public/webpack/test');
     const rscBundlePath = path.join(testBundlesDirectory, 'rsc-bundle.js');
-    const vmContext = await buildVM(rscBundlePath);
+    const executionContext = await buildExecutionContext([rscBundlePath], /* buildVmsIfNeeded */ true);
+    const vmContext = executionContext.getVMContext(rscBundlePath);
     const { ReactOnRails, React } = vmContext.context;
 
     function SuspensedComponentWithAsyncError() {

shakacode/react_on_rails

react_on_rails_pro/packages/node-renderer/tests/serverRenderRSCReactComponent.test.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import fs from 'fs';
 import { Readable } from 'stream';
-import { buildVM, getVMContext, resetVM } from '../src/worker/vm';
+import { buildExecutionContext, resetVM } from '../src/worker/vm';
 import { getConfig } from '../src/shared/configBuilder';
 
 const SimpleWorkingComponent = () => 'hello';
@@ -62,8 +62,8 @@ describe('serverRenderRSCReactComponent', () => {
   // The serverRenderRSCReactComponent function should only be called when the bundle is compiled with the `react-server` condition.
   // Therefore, we cannot call it directly in the test files. Instead, we run the RSC bundle through the VM and call the method from there.
   const getReactOnRailsRSCObject = async () => {
-    // Use the copied rsc-bundle.js file from temp directory
-    const vmContext = await buildVM(tempRscBundlePath);
+    const executionContext = await buildExecutionContext([tempRscBundlePath], /* buildVmsIfNeeded */ true);
+    const vmContext = executionContext.getVMContext(tempRscBundlePath);
     const { ReactOnRails, React } = vmContext.context;
 
     function SuspensedComponentWithAsyncError() {