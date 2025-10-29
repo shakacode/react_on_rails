efaa96f8e80db1eadca355e29b16007db7888e0e
Abanoub Ghadban <abanoub@shakacode.com>
2025-10-29 16:10:45 +0300

    serialize error during rsc payload generation at the RSC payload

--- packages/react-on-rails-pro/src/handleErrorRSC.ts
+++ packages/react-on-rails-pro/src/handleErrorRSC.ts
@@ -1,12 +1,10 @@
-import * as React from 'react';
 import { ErrorOptions } from 'react-on-rails/types';
 import { renderToPipeableStream } from 'react-on-rails-rsc/server.node';
 import generateRenderingErrorMessage from 'react-on-rails/generateRenderingErrorMessage';
 
 const handleError = (options: ErrorOptions) => {
   const msg = generateRenderingErrorMessage(options);
-  const reactElement = React.createElement('pre', null, msg);
-  return renderToPipeableStream(reactElement, {
+  return renderToPipeableStream(new Error(msg), {
     filePathToModuleMetadata: {},
     moduleLoading: { prefix: '', crossOrigin: null },
   });
