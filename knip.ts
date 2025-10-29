f78c9b4dd92e2d6c92f720cb368538f009ced54a
Abanoub Ghadban <abanoub@shakacode.com>
2025-10-29 17:08:11 +0300

    add base/full.rsc.ts to knip entry points

--- knip.ts
+++ knip.ts
@@ -42,7 +42,7 @@ const config: KnipConfig = {
 
     // React on Rails core package workspace
     'packages/react-on-rails': {
-      entry: ['src/ReactOnRails.full.ts!', 'src/ReactOnRails.client.ts!'],
+      entry: ['src/ReactOnRails.full.ts!', 'src/ReactOnRails.client.ts!', 'src/base/full.rsc.ts!'],
       project: ['src/**/*.[jt]s{x,}!', 'tests/**/*.[jt]s{x,}', '!lib/**'],
       ignore: [
         // Jest setup and test utilities - not detected by Jest plugin in workspace setup
