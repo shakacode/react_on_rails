37552c2bd1234ace22ca82dd87d9e3635d8e786f
Abanoub Ghadban <abanoub@shakacode.com>
2025-10-29 16:47:45 +0300

    fix the running scripts during oldest tests

--- packages/react-on-rails-pro/jest.config.js
+++ packages/react-on-rails-pro/jest.config.js
@@ -28,9 +28,9 @@ export default {
 
   // Allow Jest to transform react-on-rails package from node_modules
   transformIgnorePatterns: ['node_modules/(?!react-on-rails)'],
-  testEnvironmentOptions: {
-    customExportConditions: process.env.NODE_CONDITIONS?.split(',') ?? [],
-  },
+  testEnvironmentOptions: !!process.env.NODE_CONDITIONS ? {
+    customExportConditions: process.env.NODE_CONDITIONS.split(','),
+  } : {},
   // Set root directory to current package
   rootDir: '.',
 };
