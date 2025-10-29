224751f45ab22b18568a4326bdc74ae2b8bb6f0d
Abanoub Ghadban <abanoub@shakacode.com>
2025-10-29 16:21:32 +0300

    return error while streaming in one chunk

--- packages/react-on-rails-pro/src/handleError.ts
+++ packages/react-on-rails-pro/src/handleError.ts
@@ -1,12 +1,14 @@
 import * as React from 'react';
+import { Readable } from 'stream';
 import { ErrorOptions } from 'react-on-rails/types';
-import { renderToPipeableStream } from 'react-on-rails/ReactDOMServer';
+import { renderToString } from 'react-on-rails/ReactDOMServer';
 import generateRenderingErrorMessage from 'react-on-rails/generateRenderingErrorMessage';
 
 const handleError = (options: ErrorOptions) => {
   const msg = generateRenderingErrorMessage(options);
   const reactElement = React.createElement('pre', null, msg);
-  return renderToPipeableStream(reactElement);
+  const htmlString = renderToString(reactElement);
+  return Readable.from([htmlString]);
 };
 
 export default handleError;
