cf466b771ed22dd84fae241a7cf414129aab1cc1
Abanoub Ghadban <abanoub@shakacode.com>
2025-10-30 09:40:52 +0300

    reuse react on rails handleError inside rorp handleError function

--- packages/react-on-rails-pro/src/handleError.ts
+++ packages/react-on-rails-pro/src/handleError.ts
@@ -1,13 +1,9 @@
-import * as React from 'react';
 import { Readable } from 'stream';
 import { ErrorOptions } from 'react-on-rails/types';
-import { renderToString } from 'react-on-rails/ReactDOMServer';
-import generateRenderingErrorMessage from 'react-on-rails/generateRenderingErrorMessage';
+import handleErrorAsString from 'react-on-rails/handleError';
 
 const handleError = (options: ErrorOptions) => {
-  const msg = generateRenderingErrorMessage(options);
-  const reactElement = React.createElement('pre', null, msg);
-  const htmlString = renderToString(reactElement);
+  const htmlString = handleErrorAsString(options);
   return Readable.from([htmlString]);
 };
 
