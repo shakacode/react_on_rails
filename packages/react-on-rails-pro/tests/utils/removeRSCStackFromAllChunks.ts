e4ae8b4c9bf87b98e3f63adedd4df53f345bd64a
Abanoub Ghadban <abanoub@shakacode.com>
2025-10-30 20:09:57 +0300

    document bug behavior in React

---
+++ packages/react-on-rails-pro/tests/utils/removeRSCStackFromAllChunks.ts
@@ -0,0 +1,9 @@
+import removeRSCChunkStack from "./removeRSCChunkStack.ts";
+
+const removeRSCStackFromAllChunks = (allChunks: string) => {
+  return allChunks.split('\n')
+  .map((chunk) => chunk.trim().length > 0 ? removeRSCChunkStack(chunk) : chunk)
+  .join('\n');
+}
+
+export default removeRSCStackFromAllChunks;
