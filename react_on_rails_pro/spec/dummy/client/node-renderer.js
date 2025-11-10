1cab0acb103d36f9236e10df2e99a7bd057740ac
Abanoub Ghadban <abanoub@shakacode.com>
2025-11-10 15:54:48 +0200

    revert this: increase the workers restart interval

--- react_on_rails_pro/spec/dummy/client/node-renderer.js
+++ react_on_rails_pro/spec/dummy/client/node-renderer.js
@@ -42,7 +42,7 @@ const config = {
   // be set if you wish to have automatic worker restarting, say to clear memory leaks.
 
   // time in minutes between restarting all workers
-  allWorkersRestartInterval: (env.CI ? 2 : env.RENDERER_ALL_WORKERS_RESTART_INTERVAL) || 10,
+  allWorkersRestartInterval: 100,
 
   // time in minutes between each worker restarting when restarting all workers
   delayBetweenIndividualWorkerRestarts: env.CI ? 0.01 : 1,
