shakacode/react_on_rails_pro

remove the concurrent_stream_drain config flag and always stream components concurrently

lib/react_on_rails_pro/configuration.rb
@@ -53,7 +53,6 @@ module ReactOnRailsPro
     DEFAULT_RAISE_NON_SHELL_SERVER_RENDERING_ERRORS = false
     DEFAULT_ENABLE_RSC_SUPPORT = false
     DEFAULT_RSC_PAYLOAD_GENERATION_URL_PATH = "rsc_payload/"
-    DEFAULT_CONCURRENT_STREAM_DRAIN = false
     DEFAULT_CONCURRENT_STREAM_QUEUE_CAPACITY = 64
 
     attr_accessor :renderer_url, :renderer_password, :tracing,
@@ -63,7 +62,7 @@ module ReactOnRailsPro
                   :remote_bundle_cache_adapter, :ssr_pre_hook_js, :assets_to_copy,
                   :renderer_request_retry_limit, :throw_js_errors, :ssr_timeout,
                   :profile_server_rendering_js_code, :raise_non_shell_server_rendering_errors, :enable_rsc_support,
-                  :rsc_payload_generation_url_path, :concurrent_stream_drain, :concurrent_stream_queue_capacity
+                  :rsc_payload_generation_url_path, :concurrent_stream_queue_capacity
 
     def initialize(renderer_url: nil, renderer_password: nil, server_renderer: nil, # rubocop:disable Metrics/AbcSize
                    renderer_use_fallback_exec_js: nil, prerender_caching: nil,
@@ -74,7 +73,6 @@ module ReactOnRailsPro
                    renderer_request_retry_limit: nil, throw_js_errors: nil, ssr_timeout: nil,
                    profile_server_rendering_js_code: nil, raise_non_shell_server_rendering_errors: nil,
                    enable_rsc_support: nil, rsc_payload_generation_url_path: nil,
-                   concurrent_stream_drain: DEFAULT_CONCURRENT_STREAM_DRAIN,
                    concurrent_stream_queue_capacity: DEFAULT_CONCURRENT_STREAM_QUEUE_CAPACITY)
       self.renderer_url = renderer_url
       self.renderer_password = renderer_password
@@ -98,7 +96,6 @@ module ReactOnRailsPro
       self.raise_non_shell_server_rendering_errors = raise_non_shell_server_rendering_errors
       self.enable_rsc_support = enable_rsc_support
       self.rsc_payload_generation_url_path = rsc_payload_generation_url_path
-      self.concurrent_stream_drain = concurrent_stream_drain
       self.concurrent_stream_queue_capacity = concurrent_stream_queue_capacity
     end
 

shakacode/react_on_rails

react_on_rails_pro/lib/react_on_rails_pro/configuration.rb
@@ -33,7 +33,6 @@ module ReactOnRailsPro
       rsc_bundle_js_file: Configuration::DEFAULT_RSC_BUNDLE_JS_FILE,
       react_client_manifest_file: Configuration::DEFAULT_REACT_CLIENT_MANIFEST_FILE,
       react_server_client_manifest_file: Configuration::DEFAULT_REACT_SERVER_CLIENT_MANIFEST_FILE,
-      concurrent_stream_drain: Configuration::DEFAULT_CONCURRENT_STREAM_DRAIN,
       concurrent_stream_queue_capacity: Configuration::DEFAULT_CONCURRENT_STREAM_QUEUE_CAPACITY
     )
   end
@@ -61,7 +60,6 @@ module ReactOnRailsPro
     DEFAULT_RSC_BUNDLE_JS_FILE = "rsc-bundle.js"
     DEFAULT_REACT_CLIENT_MANIFEST_FILE = "react-client-manifest.json"
     DEFAULT_REACT_SERVER_CLIENT_MANIFEST_FILE = "react-server-client-manifest.json"
-    DEFAULT_CONCURRENT_STREAM_DRAIN = false
     DEFAULT_CONCURRENT_STREAM_QUEUE_CAPACITY = 64
 
     attr_accessor :renderer_url, :renderer_password, :tracing,
@@ -72,7 +70,7 @@ module ReactOnRailsPro
                   :renderer_request_retry_limit, :throw_js_errors, :ssr_timeout,
                   :profile_server_rendering_js_code, :raise_non_shell_server_rendering_errors, :enable_rsc_support,
                   :rsc_payload_generation_url_path, :rsc_bundle_js_file, :react_client_manifest_file,
-                  :react_server_client_manifest_file, :concurrent_stream_drain, :concurrent_stream_queue_capacity
+                  :react_server_client_manifest_file, :concurrent_stream_queue_capacity
 
     def initialize(renderer_url: nil, renderer_password: nil, server_renderer: nil, # rubocop:disable Metrics/AbcSize
                    renderer_use_fallback_exec_js: nil, prerender_caching: nil,
@@ -85,7 +83,6 @@ module ReactOnRailsPro
                    enable_rsc_support: nil, rsc_payload_generation_url_path: nil,
                    rsc_bundle_js_file: nil, react_client_manifest_file: nil,
                    react_server_client_manifest_file: nil,
-                   concurrent_stream_drain: DEFAULT_CONCURRENT_STREAM_DRAIN,
                    concurrent_stream_queue_capacity: DEFAULT_CONCURRENT_STREAM_QUEUE_CAPACITY)
       self.renderer_url = renderer_url
       self.renderer_password = renderer_password
@@ -112,7 +109,6 @@ module ReactOnRailsPro
       self.rsc_bundle_js_file = rsc_bundle_js_file
       self.react_client_manifest_file = react_client_manifest_file
       self.react_server_client_manifest_file = react_server_client_manifest_file
-      self.concurrent_stream_drain = concurrent_stream_drain
       self.concurrent_stream_queue_capacity = concurrent_stream_queue_capacity
     end
 