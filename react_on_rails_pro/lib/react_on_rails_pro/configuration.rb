shakacode/react_on_rails_pro

Refactor configuration and streaming logic to use concurrent_component_streaming_buffer_size for improved memory management and validation

lib/react_on_rails_pro/configuration.rb
@@ -53,7 +53,7 @@ module ReactOnRailsPro
     DEFAULT_RAISE_NON_SHELL_SERVER_RENDERING_ERRORS = false
     DEFAULT_ENABLE_RSC_SUPPORT = false
     DEFAULT_RSC_PAYLOAD_GENERATION_URL_PATH = "rsc_payload/"
-    DEFAULT_CONCURRENT_STREAM_QUEUE_CAPACITY = 64
+    DEFAULT_CONCURRENT_COMPONENT_STREAMING_BUFFER_SIZE = 64
 
     attr_accessor :renderer_url, :renderer_password, :tracing,
                   :server_renderer, :renderer_use_fallback_exec_js, :prerender_caching,
@@ -62,7 +62,7 @@ module ReactOnRailsPro
                   :remote_bundle_cache_adapter, :ssr_pre_hook_js, :assets_to_copy,
                   :renderer_request_retry_limit, :throw_js_errors, :ssr_timeout,
                   :profile_server_rendering_js_code, :raise_non_shell_server_rendering_errors, :enable_rsc_support,
-                  :rsc_payload_generation_url_path, :concurrent_stream_queue_capacity
+                  :rsc_payload_generation_url_path, :concurrent_component_streaming_buffer_size
 
     def initialize(renderer_url: nil, renderer_password: nil, server_renderer: nil, # rubocop:disable Metrics/AbcSize
                    renderer_use_fallback_exec_js: nil, prerender_caching: nil,
@@ -73,7 +73,7 @@ module ReactOnRailsPro
                    renderer_request_retry_limit: nil, throw_js_errors: nil, ssr_timeout: nil,
                    profile_server_rendering_js_code: nil, raise_non_shell_server_rendering_errors: nil,
                    enable_rsc_support: nil, rsc_payload_generation_url_path: nil,
-                   concurrent_stream_queue_capacity: DEFAULT_CONCURRENT_STREAM_QUEUE_CAPACITY)
+                   concurrent_component_streaming_buffer_size: DEFAULT_CONCURRENT_COMPONENT_STREAMING_BUFFER_SIZE)
       self.renderer_url = renderer_url
       self.renderer_password = renderer_password
       self.server_renderer = server_renderer
@@ -96,7 +96,7 @@ module ReactOnRailsPro
       self.raise_non_shell_server_rendering_errors = raise_non_shell_server_rendering_errors
       self.enable_rsc_support = enable_rsc_support
       self.rsc_payload_generation_url_path = rsc_payload_generation_url_path
-      self.concurrent_stream_queue_capacity = concurrent_stream_queue_capacity
+      self.concurrent_component_streaming_buffer_size = concurrent_component_streaming_buffer_size
     end
 
     def setup_config_values
@@ -194,6 +194,13 @@ module ReactOnRailsPro
               "config.remote_bundle_cache_adapter must have a class method named 'upload'" \
               "which takes a single named Pathname parameter 'zipped_bundles_filepath' & returns nil"
       end
+    end
+
+    def validate_concurrent_component_streaming_buffer_size
+      return if concurrent_component_streaming_buffer_size.is_a?(Numeric) &&
+                concurrent_component_streaming_buffer_size.positive?
+
+      raise ReactOnRailsPro::Error, "config.concurrent_component_streaming_buffer_size must be set and must be a positive number"
     end
 
     def setup_renderer_password

shakacode/react_on_rails

react_on_rails_pro/lib/react_on_rails_pro/configuration.rb
@@ -33,7 +33,7 @@ module ReactOnRailsPro
       rsc_bundle_js_file: Configuration::DEFAULT_RSC_BUNDLE_JS_FILE,
       react_client_manifest_file: Configuration::DEFAULT_REACT_CLIENT_MANIFEST_FILE,
       react_server_client_manifest_file: Configuration::DEFAULT_REACT_SERVER_CLIENT_MANIFEST_FILE,
-      concurrent_stream_queue_capacity: Configuration::DEFAULT_CONCURRENT_STREAM_QUEUE_CAPACITY
+      concurrent_component_streaming_buffer_size: Configuration::DEFAULT_CONCURRENT_COMPONENT_STREAMING_BUFFER_SIZE
     )
   end
 
@@ -60,7 +60,7 @@ module ReactOnRailsPro
     DEFAULT_RSC_BUNDLE_JS_FILE = "rsc-bundle.js"
     DEFAULT_REACT_CLIENT_MANIFEST_FILE = "react-client-manifest.json"
     DEFAULT_REACT_SERVER_CLIENT_MANIFEST_FILE = "react-server-client-manifest.json"
-    DEFAULT_CONCURRENT_STREAM_QUEUE_CAPACITY = 64
+    DEFAULT_CONCURRENT_COMPONENT_STREAMING_BUFFER_SIZE = 64
 
     attr_accessor :renderer_url, :renderer_password, :tracing,
                   :server_renderer, :renderer_use_fallback_exec_js, :prerender_caching,
@@ -70,7 +70,7 @@ module ReactOnRailsPro
                   :renderer_request_retry_limit, :throw_js_errors, :ssr_timeout,
                   :profile_server_rendering_js_code, :raise_non_shell_server_rendering_errors, :enable_rsc_support,
                   :rsc_payload_generation_url_path, :rsc_bundle_js_file, :react_client_manifest_file,
-                  :react_server_client_manifest_file, :concurrent_stream_queue_capacity
+                  :react_server_client_manifest_file, :concurrent_component_streaming_buffer_size
 
     def initialize(renderer_url: nil, renderer_password: nil, server_renderer: nil, # rubocop:disable Metrics/AbcSize
                    renderer_use_fallback_exec_js: nil, prerender_caching: nil,
@@ -83,7 +83,7 @@ module ReactOnRailsPro
                    enable_rsc_support: nil, rsc_payload_generation_url_path: nil,
                    rsc_bundle_js_file: nil, react_client_manifest_file: nil,
                    react_server_client_manifest_file: nil,
-                   concurrent_stream_queue_capacity: DEFAULT_CONCURRENT_STREAM_QUEUE_CAPACITY)
+                   concurrent_component_streaming_buffer_size: DEFAULT_CONCURRENT_COMPONENT_STREAMING_BUFFER_SIZE)
       self.renderer_url = renderer_url
       self.renderer_password = renderer_password
       self.server_renderer = server_renderer
@@ -109,7 +109,7 @@ module ReactOnRailsPro
       self.rsc_bundle_js_file = rsc_bundle_js_file
       self.react_client_manifest_file = react_client_manifest_file
       self.react_server_client_manifest_file = react_server_client_manifest_file
-      self.concurrent_stream_queue_capacity = concurrent_stream_queue_capacity
+      self.concurrent_component_streaming_buffer_size = concurrent_component_streaming_buffer_size
     end
 
     def setup_config_values
@@ -207,6 +207,13 @@ module ReactOnRailsPro
               "config.remote_bundle_cache_adapter must have a class method named 'upload'" \
               "which takes a single named Pathname parameter 'zipped_bundles_filepath' & returns nil"
       end
+    end
+
+    def validate_concurrent_component_streaming_buffer_size
+      return if concurrent_component_streaming_buffer_size.is_a?(Numeric) &&
+                concurrent_component_streaming_buffer_size.positive?
+
+      raise ReactOnRailsPro::Error, "config.concurrent_component_streaming_buffer_size must be set and must be a positive number"
     end
 
     def setup_renderer_password