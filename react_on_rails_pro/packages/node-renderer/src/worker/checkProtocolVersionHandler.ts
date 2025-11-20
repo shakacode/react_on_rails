shakacode/react_on_rails_pro

Refactor request handling by consolidating prechecks
- Removed individual protocol version and authentication checks from the request handling flow.
- Introduced a new `performRequestPrechecks` function to streamline the validation process for incoming requests.
- Updated the `authenticate` and `checkProtocolVersion` functions to accept request bodies directly, enhancing modularity.
- Improved error handling by ensuring consistent response structures across precheck validations.

packages/node-renderer/src/worker/checkProtocolVersionHandler.ts
@@ -2,11 +2,14 @@
  * Logic for checking protocol version.
  * @module worker/checkProtocVersionHandler
  */
-import type { FastifyRequest } from './types';
 import packageJson from '../shared/packageJson';
 
-export = function checkProtocolVersion(req: FastifyRequest) {
-  const reqProtocolVersion = (req.body as { protocolVersion?: string }).protocolVersion;
+export interface ProtocolVersionBody {
+  protocolVersion?: string;
+}
+
+export function checkProtocolVersion(body: ProtocolVersionBody) {
+  const reqProtocolVersion = body.protocolVersion;
   if (reqProtocolVersion !== packageJson.protocolVersion) {
     return {
       headers: { 'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate' },
@@ -14,11 +17,11 @@ export = function checkProtocolVersion(req: FastifyRequest) {
       data: `Unsupported renderer protocol version ${
         reqProtocolVersion
           ? `request protocol ${reqProtocolVersion}`
-          : `MISSING with body ${JSON.stringify(req.body)}`
+          : `MISSING with body ${JSON.stringify(body)}`
       } does not match installed renderer protocol ${packageJson.protocolVersion} for version ${packageJson.version}.
 Update either the renderer or the Rails server`,
     };
   }
 
   return undefined;
-};
+}

shakacode/react_on_rails

react_on_rails_pro/packages/node-renderer/src/worker/checkProtocolVersionHandler.ts
@@ -2,7 +2,6 @@
  * Logic for checking protocol version.
  * @module worker/checkProtocVersionHandler
  */
-import type { FastifyRequest } from './types';
 import packageJson from '../shared/packageJson';
 import log from '../shared/log';
 
@@ -41,8 +40,8 @@ interface RequestBody {
   railsEnv?: string;
 }
 
-export = function checkProtocolVersion(req: FastifyRequest) {
-  const { protocolVersion: reqProtocolVersion, gemVersion, railsEnv } = req.body as RequestBody;
+export function checkProtocolVersion(body: RequestBody) {
+  const { protocolVersion: reqProtocolVersion, gemVersion, railsEnv } = body;
 
   // Check protocol version
   if (reqProtocolVersion !== packageJson.protocolVersion) {
@@ -52,7 +51,7 @@ export = function checkProtocolVersion(req: FastifyRequest) {
       data: `Unsupported renderer protocol version ${
         reqProtocolVersion
           ? `request protocol ${reqProtocolVersion}`
-          : `MISSING with body ${JSON.stringify(req.body)}`
+          : `MISSING with body ${JSON.stringify(body)}`
       } does not match installed renderer protocol ${packageJson.protocolVersion} for version ${packageJson.version}.
 Update either the renderer or the Rails server`,
     };
@@ -108,4 +107,4 @@ Update either the gem or the node renderer package to match versions.`,
   }
 
   return undefined;
-};
+}