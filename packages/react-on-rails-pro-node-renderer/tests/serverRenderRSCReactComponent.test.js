shakacode/react_on_rails_pro

make buildVM returns the built vm

packages/node-renderer/tests/serverRenderRSCReactComponent.test.js
@@ -33,8 +33,7 @@ describe('serverRenderRSCReactComponent', () => {
   const getReactOnRailsRSCObject = async () => {
     const testBundlesDirectory = path.join(__dirname, '../../../spec/dummy/public/webpack/test');
     const rscBundlePath = path.join(testBundlesDirectory, 'rsc-bundle.js');
-    await buildVM(rscBundlePath);
-    const vmContext = getVMContext(rscBundlePath);
+    const vmContext = await buildVM(rscBundlePath);
     const { ReactOnRails, React } = vmContext.context;
 
     function SuspensedComponentWithAsyncError() {

shakacode/react_on_rails

packages/react-on-rails-pro-node-renderer/tests/serverRenderRSCReactComponent.test.js
@@ -66,8 +66,7 @@ describe('serverRenderRSCReactComponent', () => {
   // Therefore, we cannot call it directly in the test files. Instead, we run the RSC bundle through the VM and call the method from there.
   const getReactOnRailsRSCObject = async () => {
     // Use the copied rsc-bundle.js file from temp directory
-    await buildVM(tempRscBundlePath);
-    const vmContext = getVMContext(tempRscBundlePath);
+    const vmContext = await buildVM(tempRscBundlePath);
     const { ReactOnRails, React } = vmContext.context;
 
     function SuspensedComponentWithAsyncError() {