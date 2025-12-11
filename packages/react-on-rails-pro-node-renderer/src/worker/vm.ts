48a4510d5dd3a8c19b94f9cca76f1b311a2e7649
Abanoub Ghadban <abanoub@shakacode.com>
2025-12-11 10:49:07 +0200

    add AbortController to the vm context

--- packages/react-on-rails-pro-node-renderer/src/worker/vm.ts
+++ packages/react-on-rails-pro-node-renderer/src/worker/vm.ts
@@ -212,6 +212,7 @@ export async function buildVM(filePath: string) {
         // 1. docs/node-renderer/js-configuration.md
         // 2. packages/node-renderer/src/shared/configBuilder.ts
         extendContext(contextObject, {
+          AbortController,
           Buffer,
           TextDecoder,
           TextEncoder,
