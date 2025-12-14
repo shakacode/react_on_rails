1fafcfbf8e9f36632bd9b40a398211805836346c
Abanoub Ghadban <abanoub@shakacode.com>
2025-12-14 12:06:05 +0200

    revert all changes to reproduce the console leakage bug on CI

--- packages/react-on-rails-pro/tests/serverRenderRSCReactComponent.rsc.test.tsx
+++ packages/react-on-rails-pro/tests/serverRenderRSCReactComponent.rsc.test.tsx
@@ -11,37 +11,31 @@ import { finished } from 'stream/promises';
 import { text } from 'stream/consumers';
 import ReactOnRails, { RailsContextWithServerStreamingCapabilities } from '../src/ReactOnRailsRSC.ts';
 
-const PromiseWrapper = async ({ promise, name, onResolved }: { promise: Promise<string>; name: string, onResolved?: () => {} }) => {
+const PromiseWrapper = async ({ promise, name }: { promise: Promise<string>; name: string }) => {
   console.log(`[${name}] Before awaitng`);
   const value = await promise;
-  if (onResolved) {
-    onResolved();
-  }
   console.log(`[${name}] After awaitng`);
   return <p>Value: {value}</p>;
 };
 
-const PromiseContainer = ({ name, onResolved, tick }: { name: string, onResolved?: () => {}, tick?: () => {} }) => {
+const PromiseContainer = ({ name }: { name: string }) => {
   const promise = new Promise<string>((resolve) => {
     let i = 0;
-    setTimeout(() => {
-      const intervalId = setInterval(() => {
-        console.log(`Interval ${i} at [${name}]`);
-        i += 1;
-        tick?.();
-        if (i === 50) {
-          clearInterval(intervalId);
-          resolve(`Value of name ${name}`);
-        }
-      }, 20);
-    }, 0);
+    const intervalId = setInterval(() => {
+      console.log(`Interval ${i} at [${name}]`);
+      i += 1;
+      if (i === 50) {
+        clearInterval(intervalId);
+        resolve(`Value of name ${name}`);
+      }
+    }, 1);
   });
 
   return (
     <div>
       <h1>Initial Header</h1>
       <Suspense fallback={<p>Loading Promise</p>}>
-        <PromiseWrapper name={name} promise={promise} onResolved={onResolved} />
+        <PromiseWrapper name={name} promise={promise} />
       </Suspense>
     </div>
   );
@@ -129,8 +123,6 @@ test('no logs lekage from outside the component', async () => {
 });
 
 test('[bug] catches logs outside the component during reading the stream', async () => {
-  let resolved = false;
-  let executedIntervals = 0;
   const readable1 = ReactOnRails.serverRenderRSCReactComponent({
     railsContext: {
       reactClientManifestFileName: 'react-client-manifest.json',
@@ -140,20 +132,16 @@ test('[bug] catches logs outside the component during reading the stream', async
     renderingReturnsPromises: true,
     throwJsErrors: true,
     domNodeId: 'dom-id',
-    props: { name: 'Bug Investigation Name', onResolved: () => { resolved = true; }, tick: () => { executedIntervals += 1 } },
+    props: { name: 'First Unique Name' },
   });
 
   let content1 = '';
   let i = 0;
   readable1.on('data', (chunk: Buffer) => {
     i += 1;
-    if (i === 1) {
-      expect(resolved).toBe(false);
-    }
     // To avoid infinite loop
     if (i < 5) {
       console.log('Outside The Component');
-      console.log(`Interval Count: ${executedIntervals}`);
     }
     content1 += chunk.toString();
   });
@@ -164,10 +152,9 @@ test('[bug] catches logs outside the component during reading the stream', async
   }, 2);
   await finished(readable1);
   clearInterval(intervalId);
-  expect(resolved).toBe(true);
 
-  expect(content1).toContain('Bug Investigation Name');
+  expect(content1).toContain('First Unique Name');
   expect(content1).not.toContain('From Interval');
   // Here's the bug
   expect(content1).toContain('Outside The Component');
-}, 10000);
+});
