3da9f849f1075cb2d3c288a5b766cba73be822ce
Justin Gordon <justin@shakacode.com>
2026-02-10 17:40:48 -1000

    Bump version to 16.4.0.rc.0

--- react_on_rails/lib/react_on_rails/version.rb
+++ react_on_rails/lib/react_on_rails/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module ReactOnRails
-  VERSION = "16.3.0"
+  VERSION = "16.4.0.rc.0"
 end
--- react_on_rails_pro/lib/react_on_rails_pro/version.rb
+++ react_on_rails_pro/lib/react_on_rails_pro/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module ReactOnRailsPro
-  VERSION = "16.3.0"
+  VERSION = "16.4.0.rc.0"
   PROTOCOL_VERSION = "2.0.0"
 end
