4d6b0f9a100cf6d19f39530f5540ab8531bc4b88
ihabadham <ihabadham4p@gmail.com>
2026-02-24 00:05:42 +0200

    chore: rename hello_world layout in Pro execjs-compatible-dummy fixture

--- react_on_rails_pro/spec/execjs-compatible-dummy/app/controllers/hello_world_controller.rb
+++ react_on_rails_pro/spec/execjs-compatible-dummy/app/controllers/hello_world_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class HelloWorldController < ApplicationController
-  layout "hello_world"
+  layout "react_on_rails_default"
 
   def index
     @hello_world_props = { name: "Stranger" }
