f7e999ee2f353aa1e369b895f47aa23281a280c1
ihabadham <ihabadham4p@gmail.com>
2026-06-04 21:19:24 +0300

    Type Turbo Stream dummy flow

---
+++ react_on_rails/spec/dummy/client/app/startup/HelloTurboStream.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import RailsContext, { type RailsContextForDisplay } from '../components/RailsContext';
+
+import css from '../components/HelloWorld.module.scss';
+
+type HelloTurboStreamData = Record<string, unknown> & {
+  name: string;
+};
+
+type HelloTurboStreamProps = {
+  helloTurboStreamData: HelloTurboStreamData;
+  railsContext?: RailsContextForDisplay;
+};
+
+const HelloTurboStream = ({ helloTurboStreamData, railsContext }: HelloTurboStreamProps) => (
+  <div>
+    <h3 className={css.brightColor}>Hello, {helloTurboStreamData.name}!</h3>
+    {railsContext && <RailsContext {...{ railsContext }} />}
+  </div>
+);
+
+export default HelloTurboStream;
