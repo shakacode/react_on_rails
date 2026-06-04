4cf89badf285216eb534ad581d963b4477e3455a
ihabadham <ihabadham4p@gmail.com>
2026-06-04 20:55:30 +0300

    Type HelloWorld props dummy flow

--- react_on_rails/spec/dummy/client/app/startup/HelloWorldProps.tsx
+++ react_on_rails/spec/dummy/client/app/startup/HelloWorldProps.tsx
@@ -1,13 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+
 import css from '../components/HelloWorld.module.scss';
 
-function HelloWorldProps({ helloWorldData, modificationTarget }) {
+import type { HelloWorldData } from './HelloWorld';
+
+type Props = {
+  helloWorldData: HelloWorldData;
+  modificationTarget: string;
+};
+
+function HelloWorldProps({ helloWorldData, modificationTarget }: Props) {
   console.log(`HelloWorldProps modification target prop value: ${modificationTarget}`);
 
   const [name, setName] = useState(helloWorldData.name);
   // a trick to display a client-only prop value without creating a server/client conflict
-  const [delayedValue, setDelayedValue] = useState(null);
+  const [delayedValue, setDelayedValue] = useState<string | null>(null);
 
   useEffect(() => {
     setDelayedValue(modificationTarget);
@@ -18,18 +25,16 @@ function HelloWorldProps({ helloWorldData, modificationTarget }) {
       <h3 className={css.brightColor}>Hello, {name}!</h3>
       <p>
         Say hello to:
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <input
+          type="text"
+          value={name}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => setName(event.currentTarget.value)}
+        />
       </p>
       <h4>Value of modification target prop: {delayedValue}</h4>
     </div>
   );
 }
 
-HelloWorldProps.propTypes = {
-  helloWorldData: PropTypes.shape({
-    name: PropTypes.string,
-  }).isRequired,
-  modificationTarget: PropTypes.string.isRequired,
-};
-
+export type { Props as HelloWorldPropsProps };
 export default HelloWorldProps;
