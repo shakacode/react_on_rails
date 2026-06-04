d1fef75c2ada0dbbb9e9e4446bcd2909c33cc413
ihabadham <ihabadham4p@gmail.com>
2026-06-04 19:31:52 +0300

    Wire dummy client into TypeScript checks

---
+++ react_on_rails/spec/dummy/client/app/types/assets.d.ts
@@ -0,0 +1,24 @@
+declare module '*.module.css' {
+  const classes: Record<string, string>;
+  export default classes;
+}
+
+declare module '*.module.scss' {
+  const classes: Record<string, string>;
+  export default classes;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.ttf' {
+  const src: string;
+  export default src;
+}
