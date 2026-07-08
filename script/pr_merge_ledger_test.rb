9946f53afe2b96409e047ef85e7996e22712e1d8
Justin Gordon <justin@shakacode.com>
2026-07-07 22:12:03 -1000

    Block closeout keywords inside HTML blocks

--- script/pr_merge_ledger_test.rb
+++ script/pr_merge_ledger_test.rb
@@ -803,6 +803,39 @@ class PrMergeLedgerClosingKeywordTest < Minitest::Test
     assert_match(/Fixes #4410/, violation.fetch("message"))
   end
 
+  def test_html_comment_closing_keyword_blocks_strict_closeout
+    output, status = run_fixture(fixture_with_body("<!-- Fixes #4410 -->\n"))
+
+    refute status.success?, output
+    data = JSON.parse(output)
+    assert_equal ["code_formatted_closing_keyword"], violation_codes(data)
+    violation = ledger(data).fetch("violations").first
+    assert_equal 1, violation.fetch("line")
+    assert_match(/Fixes #4410/, violation.fetch("message"))
+  end
+
+  def test_multiline_html_comment_closing_keyword_blocks_strict_closeout
+    output, status = run_fixture(fixture_with_body("<!--\nFixes #4410\n-->\n"))
+
+    refute status.success?, output
+    data = JSON.parse(output)
+    assert_equal ["code_formatted_closing_keyword"], violation_codes(data)
+    violation = ledger(data).fetch("violations").first
+    assert_equal 2, violation.fetch("line")
+    assert_match(/Fixes #4410/, violation.fetch("message"))
+  end
+
+  def test_html_block_closing_keyword_blocks_strict_closeout
+    output, status = run_fixture(fixture_with_body("<div>\nFixes #4410\n</div>\n"))
+
+    refute status.success?, output
+    data = JSON.parse(output)
+    assert_equal ["code_formatted_closing_keyword"], violation_codes(data)
+    violation = ledger(data).fetch("violations").first
+    assert_equal 2, violation.fetch("line")
+    assert_match(/Fixes #4410/, violation.fetch("message"))
+  end
+
   def test_gfm_table_before_indented_code_blocks_strict_closeout
     output, status = run_fixture(fixture_with_body("A | B\n--|--\n1 | 2\n    Fixes #4410\n"))
 
