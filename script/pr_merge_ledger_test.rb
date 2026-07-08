0a9583fa27bbe8c0b06e418ca805417de48e6269
Justin Gordon <justin@shakacode.com>
2026-07-07 18:24:09 -1000

    Handle same-line nested list closeout keywords

--- script/pr_merge_ledger_test.rb
+++ script/pr_merge_ledger_test.rb
@@ -1159,6 +1159,17 @@ class PrMergeLedgerClosingKeywordTest < Minitest::Test
     assert_match(/Fixes #4410/, violation.fetch("message"))
   end
 
+  def test_same_line_nested_list_marker_indented_code_blocks_strict_closeout
+    output, status = run_fixture(fixture_with_body("- -     Fixes #4410\n"))
+
+    refute status.success?, output
+    data = JSON.parse(output)
+    assert_equal ["code_formatted_closing_keyword"], violation_codes(data)
+    violation = ledger(data).fetch("violations").first
+    assert_equal 1, violation.fetch("line")
+    assert_match(/Fixes #4410/, violation.fetch("message"))
+  end
+
   def test_list_blockquote_tab_indented_code_closing_keyword_blocks_strict_closeout
     output, status = run_fixture(fixture_with_body("- > \tFixes #4410\n"))
 
