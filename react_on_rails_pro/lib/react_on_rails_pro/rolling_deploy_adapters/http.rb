7f9a712368482bd973ec776e3846f68c1005bd57
Justin Gordon <justin@shakacode.com>
2026-08-17 00:54:05 -0600

    Fix HTTP failover after candidate errors

--- react_on_rails_pro/lib/react_on_rails_pro/rolling_deploy_adapters/http.rb
+++ react_on_rails_pro/lib/react_on_rails_pro/rolling_deploy_adapters/http.rb
@@ -171,7 +171,14 @@ module ReactOnRailsPro
 
             FileUtils.rm_rf(dir)
             FileUtils.mkdir_p(dir)
-            result = download_from_origin(candidate[:base], bundle_hash, dir:, deadline:)
+            begin
+              result = download_from_origin(candidate[:base], bundle_hash, dir:, deadline:)
+            rescue StandardError => e
+              Rails.logger.warn(
+                "#{LOG_PREFIX} candidate #{candidate[:base]} failed: #{e.class}: #{e.message}; trying next origin."
+              )
+              next
+            end
             next unless result
             next unless acceptable_payload?(candidate, result, bundle_hash)
 
